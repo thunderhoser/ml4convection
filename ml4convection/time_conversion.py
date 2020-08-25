bb4b4107a3e2d3d57ac259bcc692edbbc4f80a07
thunderhoser <lagerqui@ualberta.ca>
2020-08-24 18:30:30 -0600

    Added missing method.

--- ml4convection/time_conversion.py
+++ ml4convection/time_conversion.py
@@ -10,7 +10,11 @@ THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
 ))
 sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
 
+import time_periods
 import error_checking
+
+SPC_DATE_FORMAT = '%Y%m%d'
+DAYS_TO_SECONDS = 86400
 
 
 def string_to_unix_sec(time_string, time_directive):
@@ -68,3 +72,26 @@ def first_and_last_times_in_year(year):
         string_to_unix_sec(start_time_string, time_format),
         string_to_unix_sec(end_time_string, time_format)
     )
+
+
+def get_spc_dates_in_range(first_spc_date_string, last_spc_date_string):
+    """Returns list of SPC dates in range.
+
+    :param first_spc_date_string: First SPC date in range (format "yyyymmdd").
+    :param last_spc_date_string: Last SPC date in range (format "yyyymmdd").
+    :return: spc_date_strings: 1-D list of SPC dates (format "yyyymmdd").
+    """
+
+    first_spc_date_unix_sec = string_to_unix_sec(
+        first_spc_date_string, SPC_DATE_FORMAT)
+    last_spc_date_unix_sec = string_to_unix_sec(
+        last_spc_date_string, SPC_DATE_FORMAT)
+    error_checking.assert_is_geq(
+        last_spc_date_unix_sec, first_spc_date_unix_sec)
+
+    spc_dates_unix_sec = time_periods.range_and_interval_to_list(
+        start_time_unix_sec=first_spc_date_unix_sec,
+        end_time_unix_sec=last_spc_date_unix_sec,
+        time_interval_sec=DAYS_TO_SECONDS, include_endpoint=True)
+
+    return [unix_sec_to_string(t, SPC_DATE_FORMAT) for t in spc_dates_unix_sec]
