dcb86abfe84e02864ec55e46fb8f02723ed0f2c4
thunderhoser <lagerqui@ualberta.ca>
2020-09-08 18:23:56 -0600

    Added debugging print statements to neural_net.py.

--- ml4convection/neural_net.py
+++ ml4convection/neural_net.py
@@ -390,6 +390,9 @@ def _read_preprocessed_inputs_one_day(
 
     predictor_dict = example_io.concat_predictor_data(predictor_dicts)
 
+    print(target_dict[example_io.LATITUDES_KEY])
+    print('\n\n')
+    print(predictor_dict[example_io.LATITUDES_KEY])
     assert numpy.allclose(
         target_dict[example_io.LATITUDES_KEY],
         predictor_dict[example_io.LATITUDES_KEY],
