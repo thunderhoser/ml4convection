4622bec550d54d3e4b0f71807b7584a92ef4fbe6
thunderhoser <lagerqui@ualberta.ca>
2020-09-18 17:12:25 -0600

    process_radar_data.py should now be much faster and handle 3-D data.

--- ml4convection/radar_io.py
+++ ml4convection/radar_io.py
@@ -2,19 +2,23 @@
 
 import os
 import sys
+import gzip
+import shutil
 import numpy
 import netCDF4
-import time_conversion
-import longitude_conversion as lng_conversion
-import file_system_utils
-import error_checking
 
 THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
     os.path.join(os.getcwd(), os.path.expanduser(__file__))
 ))
 sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
 
+import time_conversion
+import longitude_conversion as lng_conversion
+import file_system_utils
+import error_checking
+
 TOLERANCE = 1e-6
+GZIP_FILE_EXTENSION = '.gz'
 
 DATE_FORMAT = '%Y%m%d'
 DAYS_TO_SECONDS = 86400
@@ -22,42 +26,56 @@ DAYS_TO_SECONDS = 86400
 TIME_DIMENSION_KEY = 'time'
 ROW_DIMENSION_KEY = 'row'
 COLUMN_DIMENSION_KEY = 'column'
+HEIGHT_DIMENSION_KEY = 'height'
 
 VALID_TIMES_KEY = 'valid_times_unix_sec'
 LATITUDES_KEY = 'latitudes_deg_n'
 LONGITUDES_KEY = 'longitudes_deg_e'
-COMPOSITE_REFL_KEY = 'composite_refl_matrix_dbz'
+HEIGHTS_KEY = 'heights_m_asl'
+REFLECTIVITY_KEY = 'reflectivity_matrix_dbz'
 
-ONE_PER_EXAMPLE_KEYS = [VALID_TIMES_KEY, COMPOSITE_REFL_KEY]
+ONE_PER_EXAMPLE_KEYS = [VALID_TIMES_KEY, REFLECTIVITY_KEY]
 
 
 def find_file(
-        top_directory_name, valid_date_string, with_3d=False,
-        raise_error_if_missing=True):
+        top_directory_name, valid_date_string, prefer_zipped=True,
+        allow_other_format=True, raise_error_if_missing=True):
     """Finds NetCDF file with radar data.
 
     :param top_directory_name: Name of top-level directory where file is
         expected.
     :param valid_date_string: Valid date (format "yyyymmdd").
-    :param with_3d: Boolean flag.  If True, will look for file with 3-D data.
-        If False, will look for file with 2-D data (composite reflectivity).
+    :param prefer_zipped: Boolean flag.  If True, will look for zipped file
+        first.  If False, will look for unzipped file first.
+    :param allow_other_format: Boolean flag.  If True, will allow opposite of
+        preferred file format (zipped or unzipped).
     :param raise_error_if_missing: Boolean flag.  If file is missing and
         `raise_error_if_missing == True`, will throw error.  If file is missing
         and `raise_error_if_missing == False`, will return *expected* file path.
     :return: radar_file_name: File path.
-    :raises: ValueError: if file is missing
-        and `raise_error_if_missing == True`.
+    :raises: ValueError: if file is missing and
+        `raise_error_if_missing == True`.
     """
 
     error_checking.assert_is_string(top_directory_name)
-    error_checking.assert_is_boolean(with_3d)
+    error_checking.assert_is_boolean(prefer_zipped)
+    error_checking.assert_is_boolean(allow_other_format)
     error_checking.assert_is_boolean(raise_error_if_missing)
     _ = time_conversion.string_to_unix_sec(valid_date_string, DATE_FORMAT)
 
-    radar_file_name = '{0:s}/{1:s}/{2:d}d-radar_{3:s}.nc'.format(
-        top_directory_name, valid_date_string[:4], 3 if with_3d else 2,
-        valid_date_string
+    radar_file_name = '{0:s}/{1:s}/radar_{2:s}.nc{3:s}'.format(
+        top_directory_name, valid_date_string[:4], valid_date_string,
+        GZIP_FILE_EXTENSION if prefer_zipped else ''
     )
+
+    if os.path.isfile(radar_file_name):
+        return radar_file_name
+
+    if allow_other_format:
+        if prefer_zipped:
+            radar_file_name = radar_file_name[:-len(GZIP_FILE_EXTENSION)]
+        else:
+            radar_file_name += GZIP_FILE_EXTENSION
 
     if os.path.isfile(radar_file_name) or not raise_error_if_missing:
         return radar_file_name
@@ -78,16 +96,16 @@ def file_name_to_date(radar_file_name):
 
     error_checking.assert_is_string(radar_file_name)
     pathless_file_name = os.path.split(radar_file_name)[-1]
-    extensionless_file_name = os.path.splitext(pathless_file_name)[0]
 
-    valid_date_string = extensionless_file_name.split('_')[-1]
+    valid_date_string = pathless_file_name.split('.')[0].split('_')[1]
     _ = time_conversion.string_to_unix_sec(valid_date_string, DATE_FORMAT)
 
     return valid_date_string
 
 
 def find_many_files(
-        top_directory_name, first_date_string, last_date_string, with_3d=False,
+        top_directory_name, first_date_string, last_date_string,
+        prefer_zipped=True, allow_other_format=True,
         raise_error_if_all_missing=True, raise_error_if_any_missing=False,
         test_mode=False):
     """Finds many NetCDF files with radar data.
@@ -95,7 +113,8 @@ def find_many_files(
     :param top_directory_name: See doc for `find_file`.
     :param first_date_string: First valid date (format "yyyymmdd").
     :param last_date_string: Last valid date (format "yyyymmdd").
-    :param with_3d: See doc for `find_file`.
+    :param prefer_zipped: See doc for `find_file`.
+    :param allow_other_format: Same.
     :param raise_error_if_any_missing: Boolean flag.  If any file is missing and
         `raise_error_if_any_missing == True`, will throw error.
     :param raise_error_if_all_missing: Boolean flag.  If all files are missing
@@ -120,7 +139,8 @@ def find_many_files(
     for this_date_string in valid_date_strings:
         this_file_name = find_file(
             top_directory_name=top_directory_name,
-            valid_date_string=this_date_string, with_3d=with_3d,
+            valid_date_string=this_date_string,
+            prefer_zipped=prefer_zipped, allow_other_format=allow_other_format,
             raise_error_if_missing=raise_error_if_any_missing
         )
 
@@ -139,25 +159,32 @@ def find_many_files(
     return radar_file_names
 
 
-def write_2d_file(
-        netcdf_file_name, composite_refl_matrix_dbz, latitudes_deg_n,
-        longitudes_deg_e, valid_time_unix_sec, append):
-    """Writes 2-D radar data (composite reflectivity) to NetCDF file.
+def write_file(
+        netcdf_file_name, reflectivity_matrix_dbz, latitudes_deg_n,
+        longitudes_deg_e, heights_m_asl, valid_time_unix_sec, append):
+    """Writes radar data to NetCDF file.
 
     M = number of rows in grid
     N = number of columns in grid
+    H = number of heights in grid
 
     :param netcdf_file_name: Path to output file.
-    :param composite_refl_matrix_dbz: M-by-N numpy array of composite
-        (column-maximum) reflectivities.
+    :param reflectivity_matrix_dbz: M-by-N-by-H numpy array of reflectivities.
     :param latitudes_deg_n: length-M numpy array of latitudes (deg N).
     :param longitudes_deg_e: length-N numpy array of longitudes (deg E).
+    :param heights_m_asl: length-H numpy array of heights (metres above sea
+        level).
+    :param valid_time_unix_sec: Valid time.
     :param valid_time_unix_sec: Valid time.
     :param append: Boolean flag.  If True, will append to file if file already
         exists.  If False, will create new file, overwriting if necessary.
+    :raises: ValueError: if output file is a gzip file.
     """
 
-    # Check input args.
+    if netcdf_file_name.endswith(GZIP_FILE_EXTENSION):
+        raise ValueError('Output file must not be gzip file.')
+
+    # Check valid time.
     valid_date_string = file_name_to_date(netcdf_file_name)
     date_start_time_unix_sec = (
         time_conversion.string_to_unix_sec(valid_date_string, DATE_FORMAT)
@@ -168,6 +195,7 @@ def write_2d_file(
     error_checking.assert_is_geq(valid_time_unix_sec, date_start_time_unix_sec)
     error_checking.assert_is_leq(valid_time_unix_sec, date_end_time_unix_sec)
 
+    # Check coordinates.
     error_checking.assert_is_numpy_array(latitudes_deg_n, num_dimensions=1)
     error_checking.assert_is_valid_lat_numpy_array(latitudes_deg_n)
 
@@ -176,11 +204,19 @@ def write_2d_file(
         lng_conversion.convert_lng_positive_in_west(longitudes_deg_e)
     )
 
+    error_checking.assert_is_numpy_array(heights_m_asl, num_dimensions=1)
+    error_checking.assert_is_geq_numpy_array(heights_m_asl, 0.)
+
+    # Check other input args.
     num_grid_rows = len(latitudes_deg_n)
     num_grid_columns = len(longitudes_deg_e)
-    expected_dim = numpy.array([num_grid_rows, num_grid_columns], dtype=int)
+    num_grid_heights = len(heights_m_asl)
+
+    expected_dim = numpy.array(
+        [num_grid_rows, num_grid_columns, num_grid_heights], dtype=int
+    )
     error_checking.assert_is_numpy_array(
-        composite_refl_matrix_dbz, exact_dimensions=expected_dim
+        reflectivity_matrix_dbz, exact_dimensions=expected_dim
     )
 
     error_checking.assert_is_boolean(append)
@@ -199,10 +235,13 @@ def write_2d_file(
             numpy.array(dataset_object.variables[LATITUDES_KEY][:]),
             latitudes_deg_n, atol=TOLERANCE
         )
-
         assert numpy.allclose(
             numpy.array(dataset_object.variables[LONGITUDES_KEY][:]),
             longitudes_deg_e, atol=TOLERANCE
+        )
+        assert numpy.allclose(
+            numpy.array(dataset_object.variables[HEIGHTS_KEY][:]),
+            heights_m_asl, atol=TOLERANCE
         )
 
         num_times_orig = len(numpy.array(
@@ -211,8 +250,8 @@ def write_2d_file(
         dataset_object.variables[VALID_TIMES_KEY][num_times_orig, ...] = (
             valid_time_unix_sec
         )
-        dataset_object.variables[COMPOSITE_REFL_KEY][num_times_orig, ...] = (
-            composite_refl_matrix_dbz
+        dataset_object.variables[REFLECTIVITY_KEY][num_times_orig, ...] = (
+            reflectivity_matrix_dbz
         )
 
         dataset_object.close()
@@ -226,6 +265,7 @@ def write_2d_file(
     dataset_object.createDimension(TIME_DIMENSION_KEY, None)
     dataset_object.createDimension(ROW_DIMENSION_KEY, num_grid_rows)
     dataset_object.createDimension(COLUMN_DIMENSION_KEY, num_grid_columns)
+    dataset_object.createDimension(HEIGHT_DIMENSION_KEY, num_grid_heights)
 
     dataset_object.createVariable(
         VALID_TIMES_KEY, datatype=numpy.int32, dimensions=TIME_DIMENSION_KEY
@@ -245,57 +285,105 @@ def write_2d_file(
     dataset_object.variables[LONGITUDES_KEY][:] = longitudes_deg_e
 
     dataset_object.createVariable(
-        COMPOSITE_REFL_KEY, datatype=numpy.float32,
-        dimensions=(TIME_DIMENSION_KEY, ROW_DIMENSION_KEY, COLUMN_DIMENSION_KEY)
+        HEIGHTS_KEY, datatype=numpy.float32, dimensions=HEIGHT_DIMENSION_KEY
     )
-    dataset_object.variables[COMPOSITE_REFL_KEY][:] = (
-        numpy.expand_dims(composite_refl_matrix_dbz, axis=0)
+    dataset_object.variables[HEIGHTS_KEY][:] = heights_m_asl
+
+    these_dim = (
+        TIME_DIMENSION_KEY, ROW_DIMENSION_KEY, COLUMN_DIMENSION_KEY,
+        HEIGHT_DIMENSION_KEY
+    )
+    dataset_object.createVariable(
+        REFLECTIVITY_KEY, datatype=numpy.float32, dimensions=these_dim
+    )
+    dataset_object.variables[REFLECTIVITY_KEY][:] = (
+        numpy.expand_dims(reflectivity_matrix_dbz, axis=0)
     )
 
     dataset_object.close()
 
 
-def read_2d_file(netcdf_file_name, fill_nans=True):
-    """Reads 2-D radar data (composite reflectivity) from NetCDF file.
+def read_file(netcdf_file_name, fill_nans=True):
+    """Reads radar data from NetCDF file.
 
     T = number of time steps
     M = number of rows in grid
     N = number of columns in grid
+    H = number of heights in grid
 
     :param netcdf_file_name: Path to input file.
-    :param fill_nans: Boolean flag.  If True, will use interpolation to fill NaN
-        values.
+    :param fill_nans: Boolean flag.  If True, will replace reflectivity values
+        of NaN with zero.
     :return: radar_dict: Dictionary with the following keys.
-    radar_dict['composite_refl_matrix_dbz']: T-by-M-by-N numpy array of
-        composite (column-maximum) reflectivities.
+    radar_dict['reflectivity_matrix_dbz']: T-by-M-by-N-by-H numpy array of
+        reflectivities.
     radar_dict['latitudes_deg_n']: length-M numpy array of latitudes (deg N).
     radar_dict['longitudes_deg_e']: length-N numpy array of longitudes (deg E).
+    radar_dict['heights_m_asl']: length-H numpy array of heights (metres above
+        sea level).
     radar_dict['valid_times_unix_sec']: length-T numpy array of valid times.
     """
 
     error_checking.assert_is_boolean(fill_nans)
-    dataset_object = netCDF4.Dataset(netcdf_file_name)
+    error_checking.assert_is_string(netcdf_file_name)
 
-    radar_dict = {
-        COMPOSITE_REFL_KEY: dataset_object.variables[COMPOSITE_REFL_KEY][:],
-        LATITUDES_KEY: dataset_object.variables[LATITUDES_KEY][:],
-        LONGITUDES_KEY: dataset_object.variables[LONGITUDES_KEY][:],
-        VALID_TIMES_KEY: dataset_object.variables[VALID_TIMES_KEY][:]
-    }
+    if netcdf_file_name.endswith(GZIP_FILE_EXTENSION):
+        with gzip.open(netcdf_file_name) as gzip_handle:
+            with netCDF4.Dataset(
+                    'dummy', mode='r', memory=gzip_handle.read()
+            ) as dataset_object:
+                radar_dict = {
+                    REFLECTIVITY_KEY:
+                        dataset_object.variables[REFLECTIVITY_KEY][:],
+                    LATITUDES_KEY: dataset_object.variables[LATITUDES_KEY][:],
+                    LONGITUDES_KEY: dataset_object.variables[LONGITUDES_KEY][:],
+                    HEIGHTS_KEY: dataset_object.variables[HEIGHTS_KEY][:],
+                    VALID_TIMES_KEY:
+                        dataset_object.variables[VALID_TIMES_KEY][:]
+                }
+    else:
+        dataset_object = netCDF4.Dataset(netcdf_file_name)
+
+        radar_dict = {
+            REFLECTIVITY_KEY: dataset_object.variables[REFLECTIVITY_KEY][:],
+            LATITUDES_KEY: dataset_object.variables[LATITUDES_KEY][:],
+            LONGITUDES_KEY: dataset_object.variables[LONGITUDES_KEY][:],
+            HEIGHTS_KEY: dataset_object.variables[HEIGHTS_KEY][:],
+            VALID_TIMES_KEY: dataset_object.variables[VALID_TIMES_KEY][:]
+        }
+
+        dataset_object.close()
 
     if numpy.any(numpy.diff(radar_dict[LATITUDES_KEY]) < 0):
         radar_dict[LATITUDES_KEY] = radar_dict[LATITUDES_KEY][::-1]
-        radar_dict[COMPOSITE_REFL_KEY] = numpy.flip(
-            radar_dict[COMPOSITE_REFL_KEY], axis=1
+        radar_dict[REFLECTIVITY_KEY] = numpy.flip(
+            radar_dict[REFLECTIVITY_KEY], axis=1
         )
 
     if fill_nans:
-        radar_dict[COMPOSITE_REFL_KEY][
-            numpy.isnan(radar_dict[COMPOSITE_REFL_KEY])
+        radar_dict[REFLECTIVITY_KEY][
+            numpy.isnan(radar_dict[REFLECTIVITY_KEY])
         ] = 0.
 
-    dataset_object.close()
     return radar_dict
+
+
+def compress_file(netcdf_file_name):
+    """Compresses NetCDF file with radar data (turns it into a gzip file).
+
+    :param netcdf_file_name: Path to NetCDF file.
+    :raises: ValueError: if file is already gzipped.
+    """
+
+    error_checking.assert_is_string(netcdf_file_name)
+    if netcdf_file_name.endswith(GZIP_FILE_EXTENSION):
+        raise ValueError('File must not already be gzipped.')
+
+    gzip_file_name = '{0:s}{1:s}'.format(netcdf_file_name, GZIP_FILE_EXTENSION)
+
+    with open(netcdf_file_name, 'rb') as netcdf_handle:
+        with gzip.open(gzip_file_name, 'wb') as gzip_handle:
+            shutil.copyfileobj(netcdf_handle, gzip_handle)
 
 
 def subset_by_index(radar_dict, desired_indices):
@@ -305,8 +393,6 @@ def subset_by_index(radar_dict, desired_indices):
     :param desired_indices: 1-D numpy array of desired indices.
     :return: radar_dict: Same as input but with fewer examples.
     """
-
-    # TODO(thunderhoser): Make this handle 3-D data.
 
     error_checking.assert_is_numpy_array(desired_indices, num_dimensions=1)
     error_checking.assert_is_integer_numpy_array(desired_indices)
@@ -336,8 +422,6 @@ def subset_by_time(radar_dict, desired_times_unix_sec):
     :return: radar_dict: Same as input but with fewer examples.
     :return: desired_indices: length-T numpy array of corresponding indices.
     """
-
-    # TODO(thunderhoser): Make this handle 3-D data.
 
     error_checking.assert_is_numpy_array(
         desired_times_unix_sec, num_dimensions=1
