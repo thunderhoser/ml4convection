d0316ae9aef2e3d3bfc5825f2978e3f0dafbaf48
thunderhoser <lagerqui@ualberta.ca>
2020-09-19 17:06:52 -0600

    Tweaked script.

--- ml4convection/file_renaming_shit.py
+++ ml4convection/file_renaming_shit.py
@@ -10,7 +10,7 @@ RADAR_DIR_NAME = (
 )
 
 reflectivity_file_names = glob.glob(
-    '{0:s}/201*/radar*.nc'.format(RADAR_DIR_NAME)
+    '{0:s}/201*/radar*.nc.gz'.format(RADAR_DIR_NAME)
 )
 
 for this_orig_file_name in reflectivity_file_names:
@@ -25,4 +25,4 @@ for this_orig_file_name in reflectivity_file_names:
     print('Moving "{0:s}" to "{1:s}"...'.format(
         this_orig_file_name, this_new_file_name
     ))
-    # shutil.move(this_orig_file_name, this_new_file_name)
+    shutil.move(this_orig_file_name, this_new_file_name)
