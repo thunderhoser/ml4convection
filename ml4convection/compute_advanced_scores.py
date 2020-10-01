29dd87165304b003c5c616a1609fd31e107bee3b
thunderhoser <lagerqui@ualberta.ca>
2020-09-30 19:04:22 -0600

    Added stuff.

--- ml4convection/compute_advanced_scores.py
+++ ml4convection/compute_advanced_scores.py
@@ -23,6 +23,7 @@ FIRST_DATE_ARG_NAME = 'first_date_string'
 LAST_DATE_ARG_NAME = 'last_date_string'
 MONTH_ARG_NAME = 'desired_month'
 SPLIT_BY_HOUR_ARG_NAME = 'split_by_hour'
+CLIMO_FILE_ARG_NAME = 'input_climo_file_name'
 OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 
 INPUT_DIR_HELP_STRING = (
@@ -43,6 +44,10 @@ SPLIT_BY_HOUR_HELP_STRING = (
     '[used only if `{0:s}` is left alone] Boolean flag.  If 1, will split '
     'evaluation by hour, writing one file for each hour of the day.  If 0, will'
     ' evaluate predictions for all hours.'
+)
+CLIMO_FILE_HELP_STRING = (
+    '[used only if evaluating by hour or month] Path to file with climatology '
+    '(hourly and monthly event frequencies in training data).'
 )
 OUTPUT_DIR_HELP_STRING = (
     'Name of output directory.  Results (advanced scores) will be written here '
@@ -70,13 +75,17 @@ INPUT_ARG_PARSER.add_argument(
     help=SPLIT_BY_HOUR_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
+    '--' + CLIMO_FILE_ARG_NAME, type=str, required=False, default='',
+    help=CLIMO_FILE_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
     '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
     help=OUTPUT_DIR_HELP_STRING
 )
 
 
 def _run(top_basic_score_dir_name, first_date_string, last_date_string,
-         desired_month, split_by_hour, output_dir_name):
+         desired_month, split_by_hour, climo_file_name, output_dir_name):
     """Computes advanced evaluation scores.
 
     This is effectively the main method.
@@ -86,15 +95,33 @@ def _run(top_basic_score_dir_name, first_date_string, last_date_string,
     :param last_date_string: Same.
     :param desired_month: Same.
     :param split_by_hour: Same.
+    :param climo_file_name: Same.
     :param output_dir_name: Same.
     """
 
     if desired_month <= 0:
         desired_month = None
 
+    event_frequency_for_month = None
+    event_frequency_by_hour = None
+
     if desired_month is not None:
         split_by_hour = False
         error_checking.assert_is_leq(desired_month, 12)
+
+        print('Reading event frequency for month {0:d} from: "{1:s}"...'.format(
+            desired_month, climo_file_name
+        ))
+        these_freqs = evaluation.read_climo_from_file(climo_file_name)[-1]
+        event_frequency_for_month = these_freqs[desired_month - 1]
+
+    if split_by_hour:
+        print('Reading event frequency by hour from: "{0:s}"...'.format(
+            climo_file_name
+        ))
+        event_frequency_by_hour = (
+            evaluation.read_climo_from_file(climo_file_name)[2]
+        )
 
     basic_score_file_names = evaluation.find_many_basic_score_files(
         top_directory_name=top_basic_score_dir_name,
@@ -166,6 +193,19 @@ def _run(top_basic_score_dir_name, first_date_string, last_date_string,
         advanced_score_table_xarray = evaluation.get_advanced_scores(
             basic_score_table_xarray
         )
+        t = advanced_score_table_xarray
+
+        if desired_month is not None:
+            t.attrs[evaluation.TRAINING_EVENT_FREQ_KEY] = (
+                event_frequency_for_month
+            )
+
+        if split_by_hour:
+            t.attrs[evaluation.TRAINING_EVENT_FREQ_KEY] = (
+                event_frequency_by_hour[j]
+            )
+
+        advanced_score_table_xarray = t
         print(advanced_score_table_xarray)
 
         output_file_name = evaluation.find_advanced_score_file(
@@ -190,5 +230,6 @@ if __name__ == '__main__':
         last_date_string=getattr(INPUT_ARG_OBJECT, LAST_DATE_ARG_NAME),
         desired_month=getattr(INPUT_ARG_OBJECT, MONTH_ARG_NAME),
         split_by_hour=bool(getattr(INPUT_ARG_OBJECT, SPLIT_BY_HOUR_ARG_NAME)),
+        climo_file_name=getattr(INPUT_ARG_OBJECT, CLIMO_FILE_ARG_NAME),
         output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME)
     )
