2e4a7895be88017ec2c701cb1dc4c7545708132e
thunderhoser <lagerqui@ualberta.ca>
2020-10-13 17:10:28 -0600

    Added script concat_figures.py.

--- ml4convection/concat_figures.py
+++ ml4convection/concat_figures.py
@@ -19,6 +19,7 @@ import time_conversion
 import file_system_utils
 import error_checking
 import imagemagick_utils
+import neural_net
 
 DAYS_TO_SECONDS = 86400
 TIME_FORMAT = '%Y-%m-%d-%H%M'
@@ -28,10 +29,10 @@ CONCAT_FIGURE_SIZE_PX = int(1e7)
 PREDICTION_FIGURE_DIR_ARG_NAME = 'prediction_figure_dir_name'
 REFL_FIGURE_DIR_ARG_NAME = 'refl_figure_dir_name'
 SATELLITE_FIGURE_DIR_ARG_NAME = 'satellite_figure_dir_name'
-BAND_NUMBERS_ARG_NAME = 'band_numbers'
+MODEL_METAFILE_ARG_NAME = 'model_metafile_name'
 FIRST_DATE_ARG_NAME = 'first_date_string'
 LAST_DATE_ARG_NAME = 'last_date_string'
-DAILY_TIMES_ARG_NAME = 'daily_times_seconds'
+DAILY_VALID_TIMES_ARG_NAME = 'daily_valid_times_seconds'
 OUTPUT_DIR_ARG_NAME = 'output_dir_name'
 
 PREDICTION_FIGURE_DIR_HELP_STRING = (
@@ -46,18 +47,18 @@ SATELLITE_FIGURE_DIR_HELP_STRING = (
     'Name of top-level directory (one subdirectory per spectral band) showing '
     'brightness temperatures, created by plot_satellite.py.'
 )
-BAND_NUMBERS_HELP_STRING = (
-    'List of band numbers for satellite figures.  Will use figures only for '
-    'these spectral bands.'
+MODEL_METAFILE_HELP_STRING = (
+    'Path to file with metadata for model that generated predictions.  Will be '
+    'read by `neural_net.read_metafile`.'
 )
 DATE_HELP_STRING = (
     'Date (format "yyyymmdd").  Will concatenate figures for all days in the '
     'period `{0:s}`...`{1:s}`.'
 ).format(FIRST_DATE_ARG_NAME, LAST_DATE_ARG_NAME)
 
-DAILY_TIMES_HELP_STRING = (
-    'List of times to use for each day.  All values should be in the range '
-    '0...86399.'
+DAILY_VALID_TIMES_HELP_STRING = (
+    'List of valid times (target times) to use for each day.  All values should'
+    ' be in the range 0...86399.'
 )
 OUTPUT_DIR_HELP_STRING = (
     'Name of output directory.  Concatenated (paneled) figures will be saved '
@@ -78,18 +79,18 @@ INPUT_ARG_PARSER.add_argument(
     help=SATELLITE_FIGURE_DIR_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
+    '--' + MODEL_METAFILE_ARG_NAME, type=str, required=True,
+    help=MODEL_METAFILE_HELP_STRING
+)
+INPUT_ARG_PARSER.add_argument(
     '--' + FIRST_DATE_ARG_NAME, type=str, required=True, help=DATE_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
     '--' + LAST_DATE_ARG_NAME, type=str, required=True, help=DATE_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
-    '--' + BAND_NUMBERS_ARG_NAME, type=int, nargs='+', required=True,
-    help=BAND_NUMBERS_HELP_STRING
-)
-INPUT_ARG_PARSER.add_argument(
-    '--' + DAILY_TIMES_ARG_NAME, type=int, nargs='+', required=True,
-    help=DAILY_TIMES_HELP_STRING
+    '--' + DAILY_VALID_TIMES_ARG_NAME, type=int, nargs='+', required=True,
+    help=DAILY_VALID_TIMES_HELP_STRING
 )
 INPUT_ARG_PARSER.add_argument(
     '--' + OUTPUT_DIR_ARG_NAME, type=str, required=True,
@@ -99,31 +100,35 @@ INPUT_ARG_PARSER.add_argument(
 
 def _find_input_files(
         prediction_figure_dir_name, refl_figure_dir_name,
-        satellite_figure_dir_name, band_numbers, first_date_string,
-        last_date_string, daily_times_seconds):
+        satellite_figure_dir_name, model_metafile_name, first_date_string,
+        last_date_string, daily_valid_times_seconds):
     """Finds input files (figures to concatenate).
 
-    T = number of time steps
+    V = number of valid times
     B = number of satellite bands
+    L = number of lag times
 
     :param prediction_figure_dir_name: See documentation at top of file.
     :param refl_figure_dir_name: Same.
     :param satellite_figure_dir_name: Same.
-    :param band_numbers: Same.
+    :param model_metafile_name: Same.
     :param first_date_string: Same.
     :param last_date_string: Same.
-    :param daily_times_seconds: Same.
-    :return: prediction_figure_file_names: length-T list of paths to prediction
+    :param daily_valid_times_seconds: Same.
+    :return: prediction_figure_file_names: length-V list of paths to prediction
         figures.
-    :return: refl_figure_file_names: length-T list of paths to reflectivity
+    :return: refl_figure_file_names: length-V list of paths to reflectivity
         figures.
-    :return: satellite_fig_file_name_matrix: T-by-B numpy array of paths to
+    :return: satellite_fig_file_name_matrix: V-by-B-by-L numpy array of paths to
         satellite figures.
-    :return: valid_time_strings: length-T numpy array of valid times (format
+    :return: valid_time_strings: length-V numpy array of valid times (format
         "yyyy-mm-dd-HHMM").
     :raises: ValueError: if any expected reflectivity or satellite figure is not
         found.
     """
+
+    print('Reading model metadata from: "{0:s}"...'.format(model_metafile_name))
+    model_metadata_dict = neural_net.read_metafile(model_metafile_name)
 
     date_strings = time_conversion.get_spc_dates_in_range(
         first_date_string, last_date_string
@@ -133,28 +138,34 @@ def _find_input_files(
         for d in date_strings
     ], dtype=int)
 
-    valid_times_unix_sec = numpy.concatenate([
-        d + daily_times_seconds for d in dates_unix_sec
+    possible_valid_times_unix_sec = numpy.concatenate([
+        d + daily_valid_times_seconds for d in dates_unix_sec
     ])
 
     possible_valid_time_strings = [
         time_conversion.unix_sec_to_string(t, TIME_FORMAT)
-        for t in valid_times_unix_sec
+        for t in possible_valid_times_unix_sec
     ]
     possible_pred_fig_file_names = [
         '{0:s}/predictions_{1:s}.jpg'.format(prediction_figure_dir_name, t)
         for t in possible_valid_time_strings
     ]
 
-    valid_time_strings = []
+    valid_times_unix_sec = []
     prediction_figure_file_names = []
 
     for i in range(len(possible_valid_time_strings)):
         if not os.path.isfile(possible_pred_fig_file_names[i]):
             continue
 
-        valid_time_strings.append(possible_valid_time_strings[i])
+        valid_times_unix_sec.append(possible_valid_times_unix_sec[i])
         prediction_figure_file_names.append(possible_pred_fig_file_names[i])
+
+    valid_times_unix_sec = numpy.array(valid_times_unix_sec, dtype=int)
+    valid_time_strings = [
+        time_conversion.unix_sec_to_string(t, TIME_FORMAT)
+        for t in valid_times_unix_sec
+    ]
 
     refl_figure_file_names = [
         '{0:s}/composite_reflectivity_{1:s}.jpg'.format(refl_figure_dir_name, t)
@@ -174,20 +185,32 @@ def _find_input_files(
 
         raise ValueError(error_string)
 
-    num_times = len(valid_time_strings)
+    training_option_dict = model_metadata_dict[neural_net.TRAINING_OPTIONS_KEY]
+    band_numbers = training_option_dict[neural_net.BAND_NUMBERS_KEY]
+    lag_times_seconds = training_option_dict[neural_net.LAG_TIMES_KEY]
+
+    num_valid_times = len(valid_time_strings)
     num_bands = len(band_numbers)
+    num_lag_times = len(lag_times_seconds)
+
     satellite_fig_file_name_matrix = numpy.full(
-        (num_times, num_bands), '', dtype=object
+        (num_valid_times, num_bands, num_lag_times), '', dtype=object
     )
 
-    for i in range(num_times):
+    for i in range(num_valid_times):
         for j in range(num_bands):
-            satellite_fig_file_name_matrix[i, j] = (
-                '{0:s}/band{1:02d}/brightness-temperature_band{1:02d}_{2:s}.jpg'
-            ).format(
-                satellite_figure_dir_name,
-                band_numbers[j], valid_time_strings[i]
-            )
+            for k in range(num_lag_times):
+                this_time_string = time_conversion.unix_sec_to_string(
+                    valid_times_unix_sec[i] - lag_times_seconds[k],
+                    TIME_FORMAT
+                )
+
+                satellite_fig_file_name_matrix[i, j, k] = (
+                    '{0:s}/band{1:02d}/brightness-temperature_band{1:02d}'
+                    '_{2:s}.jpg'
+                ).format(
+                    satellite_figure_dir_name, band_numbers[j], this_time_string
+                )
 
     bad_file_names = [
         f for f in numpy.ravel(satellite_fig_file_name_matrix)
@@ -213,8 +236,8 @@ def _find_input_files(
 
 
 def _run(prediction_figure_dir_name, refl_figure_dir_name,
-         satellite_figure_dir_name, band_numbers, first_date_string,
-         last_date_string, daily_times_seconds, output_dir_name):
+         satellite_figure_dir_name, model_metafile_name, first_date_string,
+         last_date_string, daily_valid_times_seconds, output_dir_name):
     """For each time step, concatenates figures with the following data:
 
     - Predictions (forecast convection probabilities) and targets
@@ -226,18 +249,17 @@ def _run(prediction_figure_dir_name, refl_figure_dir_name,
     :param prediction_figure_dir_name: See documentation at top of file.
     :param refl_figure_dir_name: Same.
     :param satellite_figure_dir_name: Same.
-    :param band_numbers: Same.
+    :param model_metafile_name: Same.
     :param first_date_string: Same.
     :param last_date_string: Same.
-    :param daily_times_seconds: Same.
+    :param daily_valid_times_seconds: Same.
     :param output_dir_name: Same.
     """
 
-    error_checking.assert_is_geq_numpy_array(daily_times_seconds, 0)
+    error_checking.assert_is_geq_numpy_array(daily_valid_times_seconds, 0)
     error_checking.assert_is_less_than_numpy_array(
-        daily_times_seconds, DAYS_TO_SECONDS
+        daily_valid_times_seconds, DAYS_TO_SECONDS
     )
-    error_checking.assert_is_greater_numpy_array(band_numbers, 0)
 
     file_system_utils.mkdir_recursive_if_necessary(
         directory_name=output_dir_name
@@ -252,14 +274,19 @@ def _run(prediction_figure_dir_name, refl_figure_dir_name,
         prediction_figure_dir_name=prediction_figure_dir_name,
         refl_figure_dir_name=refl_figure_dir_name,
         satellite_figure_dir_name=satellite_figure_dir_name,
-        band_numbers=band_numbers,
+        model_metafile_name=model_metafile_name,
         first_date_string=first_date_string,
         last_date_string=last_date_string,
-        daily_times_seconds=daily_times_seconds
+        daily_valid_times_seconds=daily_valid_times_seconds
     )
 
-    num_times = satellite_fig_file_name_matrix.shape[0]
+    model_metadata_dict = neural_net.read_metafile(model_metafile_name)
+    training_option_dict = model_metadata_dict[neural_net.TRAINING_OPTIONS_KEY]
+    lag_times_seconds = training_option_dict[neural_net.LAG_TIMES_KEY]
+
+    num_valid_times = satellite_fig_file_name_matrix.shape[0]
     num_bands = satellite_fig_file_name_matrix.shape[1]
+    num_lag_times = len(lag_times_seconds)
 
     num_panels = num_bands + 2
     num_panel_rows = int(numpy.floor(
@@ -269,29 +296,33 @@ def _run(prediction_figure_dir_name, refl_figure_dir_name,
         float(num_panels) / num_panel_rows
     ))
 
-    for i in range(num_times):
-        these_input_file_names = (
-            satellite_fig_file_name_matrix[i, :].tolist() +
-            [refl_figure_file_names[i], prediction_figure_file_names[i]]
-        )
-        this_output_file_name = '{0:s}/all_data_{1:s}.jpg'.format(
-            output_dir_name, valid_time_strings[i]
-        )
+    for i in range(num_valid_times):
+        for k in range(num_lag_times):
+            these_input_file_names = (
+                satellite_fig_file_name_matrix[i, :, k].tolist() +
+                [refl_figure_file_names[i], prediction_figure_file_names[i]]
+            )
+            this_output_file_name = (
+                '{0:s}/all_data_valid-{1:s}_lag-time-seconds={2:05d}.jpg'
+            ).format(
+                output_dir_name, valid_time_strings[i], lag_times_seconds[k]
+            )
 
-        print('Concatenating figures to: "{0:s}"...'.format(
-            this_output_file_name
-        ))
-        imagemagick_utils.concatenate_images(
-            input_file_names=these_input_file_names,
-            output_file_name=this_output_file_name,
-            num_panel_rows=num_panel_rows, num_panel_columns=num_panel_columns
-        )
+            print('Concatenating figures to: "{0:s}"...'.format(
+                this_output_file_name
+            ))
+            imagemagick_utils.concatenate_images(
+                input_file_names=these_input_file_names,
+                output_file_name=this_output_file_name,
+                num_panel_rows=num_panel_rows,
+                num_panel_columns=num_panel_columns
+            )
 
-        imagemagick_utils.resize_image(
-            input_file_name=this_output_file_name,
-            output_file_name=this_output_file_name,
-            output_size_pixels=CONCAT_FIGURE_SIZE_PX
-        )
+            imagemagick_utils.resize_image(
+                input_file_name=this_output_file_name,
+                output_file_name=this_output_file_name,
+                output_size_pixels=CONCAT_FIGURE_SIZE_PX
+            )
 
 
 if __name__ == '__main__':
@@ -307,13 +338,11 @@ if __name__ == '__main__':
         satellite_figure_dir_name=getattr(
             INPUT_ARG_OBJECT, SATELLITE_FIGURE_DIR_ARG_NAME
         ),
-        band_numbers=numpy.array(
-            getattr(INPUT_ARG_OBJECT, BAND_NUMBERS_ARG_NAME), dtype=int
-        ),
+        model_metafile_name=getattr(INPUT_ARG_OBJECT, MODEL_METAFILE_ARG_NAME),
         first_date_string=getattr(INPUT_ARG_OBJECT, FIRST_DATE_ARG_NAME),
         last_date_string=getattr(INPUT_ARG_OBJECT, LAST_DATE_ARG_NAME),
-        daily_times_seconds=numpy.array(
-            getattr(INPUT_ARG_OBJECT, DAILY_TIMES_ARG_NAME), dtype=int
+        daily_valid_times_seconds=numpy.array(
+            getattr(INPUT_ARG_OBJECT, DAILY_VALID_TIMES_ARG_NAME), dtype=int
         ),
         output_dir_name=getattr(INPUT_ARG_OBJECT, OUTPUT_DIR_ARG_NAME)
     )
