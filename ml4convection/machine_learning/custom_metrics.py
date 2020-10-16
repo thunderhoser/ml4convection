e91673acb2a5ae519709e0703f8d7dc9d19ac1c2
thunderhoser <lagerqui@ualberta.ca>
2020-10-16 13:38:24 -0600

    Added more stuff to custom_metrics.py.

--- ml4convection/machine_learning/custom_metrics.py
+++ ml4convection/machine_learning/custom_metrics.py
@@ -1,7 +1,10 @@
 """Custom metrics for Keras models."""
 
+import tensorflow
 from tensorflow.keras import backend as K
 from gewittergefahr.gg_utils import error_checking
+
+TOLERANCE = 1e-6
 
 
 def _apply_max_filter(input_tensor, half_window_size_px, test_mode):
@@ -266,3 +269,237 @@ def csi(half_window_size_px, function_name=None, test_mode=False):
         csi_function.__name__ = function_name
 
     return csi_function
+
+
+def dice_coeff(half_window_size_px, function_name=None, test_mode=False):
+    """Creates function to compute Dice coefficient.
+
+    :param half_window_size_px: See doc for `_apply_max_filter`.
+    :param function_name: Function name (string).
+    :param test_mode: Leave this alone.
+    :return: dice_function: Function (defined below).
+    """
+
+    # TODO(thunderhoser): Need _check_input_args... maybe.
+
+    error_checking.assert_is_integer(half_window_size_px)
+    error_checking.assert_is_geq(half_window_size_px, 0)
+    error_checking.assert_is_boolean(test_mode)
+
+    if function_name is not None:
+        error_checking.assert_is_string(function_name)
+
+    def dice_function(target_tensor, prediction_tensor):
+        """Computes Dice coefficient.
+
+        :param target_tensor: Tensor of target (actual) values.
+        :param prediction_tensor: Tensor of predicted values.
+        :return: dice_coeff: Dice coefficient.
+        """
+
+        filtered_target_tensor = _apply_max_filter(
+            input_tensor=target_tensor,
+            half_window_size_px=half_window_size_px, test_mode=test_mode
+        )
+        filtered_prediction_tensor = _apply_max_filter(
+            input_tensor=prediction_tensor,
+            half_window_size_px=half_window_size_px, test_mode=test_mode
+        )
+
+        intersection_tensor = K.sum(
+            filtered_target_tensor * filtered_prediction_tensor, axis=(1, 2, 3)
+        )
+        num_pixels_tensor = K.sum(
+            K.ones_like(filtered_target_tensor), axis=(1, 2, 3)
+        )
+
+        return K.mean(2 * intersection_tensor / num_pixels_tensor)
+
+    if function_name is not None:
+        dice_function.__name__ = function_name
+
+    return dice_function
+
+
+def iou(half_window_size_px, function_name=None, test_mode=False):
+    """Creates function to compute intersection over union.
+
+    :param half_window_size_px: See doc for `_apply_max_filter`.
+    :param function_name: Function name (string).
+    :param test_mode: Leave this alone.
+    :return: iou_function: Function (defined below).
+    """
+
+    error_checking.assert_is_integer(half_window_size_px)
+    error_checking.assert_is_geq(half_window_size_px, 0)
+    error_checking.assert_is_boolean(test_mode)
+
+    if function_name is not None:
+        error_checking.assert_is_string(function_name)
+
+    def iou_function(target_tensor, prediction_tensor):
+        """Computes intersection over union.
+
+        :param target_tensor: Tensor of target (actual) values.
+        :param prediction_tensor: Tensor of predicted values.
+        :return: iou: Intersection over union.
+        """
+
+        filtered_target_tensor = _apply_max_filter(
+            input_tensor=target_tensor,
+            half_window_size_px=half_window_size_px, test_mode=test_mode
+        )
+        filtered_prediction_tensor = _apply_max_filter(
+            input_tensor=prediction_tensor,
+            half_window_size_px=half_window_size_px, test_mode=test_mode
+        )
+
+        intersection_tensor = K.sum(
+            filtered_target_tensor * filtered_prediction_tensor, axis=(1, 2, 3)
+        )
+        union_tensor = (
+            K.sum(filtered_target_tensor, axis=(1, 2, 3)) +
+            K.sum(filtered_prediction_tensor, axis=(1, 2, 3)) -
+            intersection_tensor
+        )
+
+        return K.mean(
+            intersection_tensor / (union_tensor + K.epsilon())
+        )
+
+    if function_name is not None:
+        iou_function.__name__ = function_name
+
+    return iou_function
+
+
+def tversky_coeff(
+        half_window_size_px, false_positive_weight, false_negative_weight,
+        function_name=None, test_mode=False):
+    """Creates function to compute Tversky coefficient (weighted CSI).
+
+    :param half_window_size_px: See doc for `_apply_max_filter`.
+    :param false_positive_weight: Weight for false positives.
+    :param false_negative_weight: Weight for false negatives.
+    :param function_name: Function name (string).
+    :param test_mode: Leave this alone.
+    :return: tversky_function: Function (defined below).
+    """
+
+    error_checking.assert_is_integer(half_window_size_px)
+    error_checking.assert_is_geq(half_window_size_px, 0)
+    error_checking.assert_is_greater(false_positive_weight, 0.)
+    error_checking.assert_is_greater(false_negative_weight, 0.)
+    error_checking.assert_is_boolean(test_mode)
+
+    if function_name is not None:
+        error_checking.assert_is_string(function_name)
+
+    def tversky_function(target_tensor, prediction_tensor):
+        """Computes Tversky coefficient.
+
+        :param target_tensor: Tensor of target (actual) values.
+        :param prediction_tensor: Tensor of predicted values.
+        :return: tversky_coeff: Tversky coefficient.
+        """
+
+        filtered_target_tensor = _apply_max_filter(
+            input_tensor=target_tensor,
+            half_window_size_px=half_window_size_px, test_mode=test_mode
+        )
+        filtered_prediction_tensor = _apply_max_filter(
+            input_tensor=prediction_tensor,
+            half_window_size_px=half_window_size_px, test_mode=test_mode
+        )
+
+        num_true_positives = false_positive_weight * K.sum(
+            filtered_prediction_tensor * filtered_target_tensor
+        )
+        num_false_positives = false_positive_weight * K.sum(
+            filtered_prediction_tensor * (1. - filtered_target_tensor)
+        )
+        num_false_negatives = false_negative_weight * K.sum(
+            filtered_target_tensor * (1. - filtered_prediction_tensor)
+        )
+
+        denominator = (
+            num_false_positives + num_false_negatives + num_true_positives +
+            K.epsilon()
+        )
+
+        return num_true_positives / denominator
+
+    if function_name is not None:
+        tversky_function.__name__ = function_name
+
+    return tversky_function
+
+
+def focal_loss(
+        half_window_size_px, training_event_freq, focusing_factor,
+        function_name=None, test_mode=False):
+    """Creates function to compute focal loss.
+
+    Paper reference: https://arxiv.org/pdf/1708.02002.pdf
+    Code reference: https://github.com/umbertogriffo/focal-loss-keras/blob/
+                    master/src/loss_function/losses.py
+
+    :param half_window_size_px: See doc for `_apply_max_filter`.
+    :param training_event_freq: Event frequency (positive-class frequency) in
+        training data.
+    :param focusing_factor: Focusing factor (gamma in referenced paper).
+    :param function_name: Function name (string).
+    :param test_mode: Leave this alone.
+    :return: loss: Function (defined below).
+    """
+
+    error_checking.assert_is_integer(half_window_size_px)
+    error_checking.assert_is_geq(half_window_size_px, 0)
+    error_checking.assert_is_greater(training_event_freq, 0.)
+    error_checking.assert_is_less_than(training_event_freq, 1.)
+    error_checking.assert_is_geq(focusing_factor, 1.)
+    error_checking.assert_is_boolean(test_mode)
+
+    if function_name is not None:
+        error_checking.assert_is_string(function_name)
+
+    def loss(target_tensor, prediction_tensor):
+        """Computes loss.
+
+        :param target_tensor: Tensor of target (actual) values.
+        :param prediction_tensor: Tensor of predicted values.
+        :return: loss: Focal loss.
+        """
+
+        filtered_target_tensor = _apply_max_filter(
+            input_tensor=target_tensor,
+            half_window_size_px=half_window_size_px, test_mode=test_mode
+        )
+        filtered_prediction_tensor = _apply_max_filter(
+            input_tensor=prediction_tensor,
+            half_window_size_px=half_window_size_px, test_mode=test_mode
+        )
+
+        filtered_prediction_tensor = K.clip(
+            filtered_prediction_tensor, K.epsilon(), 1. - K.epsilon()
+        )
+        probability_tensor = tensorflow.where(
+            K.equal(filtered_target_tensor, 1),
+            filtered_prediction_tensor, 1. - filtered_prediction_tensor
+        )
+
+        alpha_tensor = training_event_freq * K.ones_like(filtered_target_tensor)
+        alpha_tensor = tensorflow.where(
+            K.equal(filtered_target_tensor, 1), alpha_tensor, 1. - alpha_tensor
+        )
+
+        cross_entropy_tensor = -K.log(probability_tensor)
+        weight_tensor = alpha_tensor * K.pow(
+            (1. - probability_tensor), focusing_factor
+        )
+        return K.mean(K.sum(weight_tensor * cross_entropy_tensor, axis=0))
+
+    if function_name is not None:
+        loss.__name__ = function_name
+
+    return loss
--- ml4convection/machine_learning/custom_metrics_test.py
+++ ml4convection/machine_learning/custom_metrics_test.py
@@ -6,7 +6,10 @@ import tensorflow
 from keras import backend as K
 from ml4convection.machine_learning import custom_metrics
 
+# TODO(thunderhoser): Need unit tests that check exact values.
+
 TOLERANCE = 1e-6
+DEFAULT_HALF_WINDOW_SIZE_PX = 1
 
 TARGET_MATRIX = numpy.array([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -21,7 +24,7 @@ TARGET_MATRIX = numpy.array([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ], dtype=int)
 
-PROBABILITY_MATRIX = numpy.array([
+PREDICTION_MATRIX = numpy.array([
     [0.1, 0.2, 0.3, 0.4, 0.0, 0.0, 0.0, 0.0, 0.4, 0.3, 0.2, 0.1],
     [0.5, 0.6, 0.7, 0.8, 0.0, 0.0, 0.0, 0.0, 0.8, 0.7, 0.6, 0.5],
     [0.2, 0.4, 0.6, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.6, 0.4, 0.2],
@@ -39,15 +42,13 @@ TARGET_TENSOR = tensorflow.expand_dims(TARGET_TENSOR, 0)
 TARGET_TENSOR = tensorflow.expand_dims(TARGET_TENSOR, -1)
 
 PREDICTION_TENSOR = tensorflow.constant(
-    PROBABILITY_MATRIX, dtype=tensorflow.float32
+    PREDICTION_MATRIX, dtype=tensorflow.float32
 )
 PREDICTION_TENSOR = tensorflow.expand_dims(PREDICTION_TENSOR, 0)
 PREDICTION_TENSOR = tensorflow.expand_dims(PREDICTION_TENSOR, -1)
 
 # The following constants are used to test _apply_max_filter.
-HALF_WINDOW_SIZE_PX = 1
-
-FILTERED_TARGET_MATRIX = numpy.array([
+FILTERED_TARGET_MATRIX_HW1 = numpy.array([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0],
@@ -60,7 +61,7 @@ FILTERED_TARGET_MATRIX = numpy.array([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ], dtype=int)
 
-FILTERED_PREDICTION_MATRIX = numpy.array([
+FILTERED_PREDICTION_MATRIX_HW1 = numpy.array([
     [0.6, 0.7, 0.8, 0.8, 0.8, 0.0, 0.0, 0.8, 0.8, 0.8, 0.7, 0.6],
     [0.6, 0.7, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.7, 0.6],
     [0.6, 0.7, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.7, 0.6],
@@ -77,37 +78,77 @@ FILTERED_PREDICTION_MATRIX = numpy.array([
 class CustomMetricsTests(unittest.TestCase):
     """Each method is a unit test for custom_metrics.py."""
 
-    def test_apply_max_filter_targets(self):
-        """Ensures correct output from _apply_max_filter on target matrix."""
+    def test_apply_max_filter_targets_hw0(self):
+        """Ensures correct output from _apply_max_filter.
+
+        In this case, applying filter to target matrix with half-window size =
+        0 pixels.
+        """
 
         this_tensor = custom_metrics._apply_max_filter(
-            input_tensor=TARGET_TENSOR, half_window_size_px=HALF_WINDOW_SIZE_PX,
-            test_mode=True
+            input_tensor=TARGET_TENSOR, half_window_size_px=0, test_mode=True
         )
         this_matrix = K.eval(this_tensor)[0, ..., 0]
 
         self.assertTrue(numpy.allclose(
-            this_matrix, FILTERED_TARGET_MATRIX, atol=TOLERANCE
+            this_matrix, TARGET_MATRIX, atol=TOLERANCE
         ))
 
-    def test_apply_max_filter_predictions(self):
-        """Ensures correct output from _apply_max_filter on predictions."""
+    def test_apply_max_filter_targets_hw1(self):
+        """Ensures correct output from _apply_max_filter.
+
+        In this case, applying filter to target matrix with half-window size =
+        1 pixel.
+        """
+
+        this_tensor = custom_metrics._apply_max_filter(
+            input_tensor=TARGET_TENSOR, half_window_size_px=1, test_mode=True
+        )
+        this_matrix = K.eval(this_tensor)[0, ..., 0]
+
+        self.assertTrue(numpy.allclose(
+            this_matrix, FILTERED_TARGET_MATRIX_HW1, atol=TOLERANCE
+        ))
+
+    def test_apply_max_filter_predictions_hw0(self):
+        """Ensures correct output from _apply_max_filter on predictions.
+
+        In this case, applying filter to prediction matrix with half-window
+        size = 0 pixels.
+        """
 
         this_tensor = custom_metrics._apply_max_filter(
             input_tensor=PREDICTION_TENSOR,
-            half_window_size_px=HALF_WINDOW_SIZE_PX, test_mode=True
+            half_window_size_px=0, test_mode=True
         )
         this_matrix = K.eval(this_tensor)[0, ..., 0]
 
         self.assertTrue(numpy.allclose(
-            this_matrix, FILTERED_PREDICTION_MATRIX, atol=TOLERANCE
+            this_matrix, PREDICTION_MATRIX, atol=TOLERANCE
+        ))
+
+    def test_apply_max_filter_predictions_hw1(self):
+        """Ensures correct output from _apply_max_filter on predictions.
+
+        In this case, applying filter to prediction matrix with half-window
+        size = 1 pixel.
+        """
+
+        this_tensor = custom_metrics._apply_max_filter(
+            input_tensor=PREDICTION_TENSOR,
+            half_window_size_px=1, test_mode=True
+        )
+        this_matrix = K.eval(this_tensor)[0, ..., 0]
+
+        self.assertTrue(numpy.allclose(
+            this_matrix, FILTERED_PREDICTION_MATRIX_HW1, atol=TOLERANCE
         ))
 
     def test_pod(self):
         """Ensures that pod() does not crash."""
 
         this_function = custom_metrics.pod(
-            half_window_size_px=HALF_WINDOW_SIZE_PX, test_mode=True
+            half_window_size_px=DEFAULT_HALF_WINDOW_SIZE_PX, test_mode=True
         )
         this_pod = K.eval(this_function(TARGET_TENSOR, PREDICTION_TENSOR))
 
@@ -117,7 +158,7 @@ class CustomMetricsTests(unittest.TestCase):
         """Ensures that success_ratio() does not crash."""
 
         this_function = custom_metrics.success_ratio(
-            half_window_size_px=HALF_WINDOW_SIZE_PX, test_mode=True
+            half_window_size_px=DEFAULT_HALF_WINDOW_SIZE_PX, test_mode=True
         )
         this_success_ratio = K.eval(
             this_function(TARGET_TENSOR, PREDICTION_TENSOR)
@@ -129,7 +170,7 @@ class CustomMetricsTests(unittest.TestCase):
         """Ensures that frequency_bias() does not crash."""
 
         this_function = custom_metrics.frequency_bias(
-            half_window_size_px=HALF_WINDOW_SIZE_PX, test_mode=True
+            half_window_size_px=DEFAULT_HALF_WINDOW_SIZE_PX, test_mode=True
         )
         this_frequency_bias = K.eval(
             this_function(TARGET_TENSOR, PREDICTION_TENSOR)
@@ -142,11 +183,59 @@ class CustomMetricsTests(unittest.TestCase):
         """Ensures that csi() does not crash."""
 
         this_function = custom_metrics.csi(
-            half_window_size_px=HALF_WINDOW_SIZE_PX, test_mode=True
+            half_window_size_px=DEFAULT_HALF_WINDOW_SIZE_PX, test_mode=True
         )
         this_csi = K.eval(this_function(TARGET_TENSOR, PREDICTION_TENSOR))
 
         self.assertTrue(0 <= this_csi <= 1)
+
+    def test_dice_coeff(self):
+        """Ensures that dice_coeff() does not crash."""
+
+        this_function = custom_metrics.dice_coeff(
+            half_window_size_px=DEFAULT_HALF_WINDOW_SIZE_PX, test_mode=True
+        )
+        this_dice_coeff = K.eval(
+            this_function(TARGET_TENSOR, PREDICTION_TENSOR)
+        )
+
+        self.assertTrue(0 <= this_dice_coeff <= 1)
+
+    def test_iou(self):
+        """Ensures that iou() does not crash."""
+
+        this_function = custom_metrics.iou(
+            half_window_size_px=DEFAULT_HALF_WINDOW_SIZE_PX, test_mode=True
+        )
+        this_iou = K.eval(this_function(TARGET_TENSOR, PREDICTION_TENSOR))
+
+        self.assertTrue(0 <= this_iou <= 1)
+
+    def test_tversky_coeff(self):
+        """Ensures that tversky_coeff() does not crash."""
+
+        this_function = custom_metrics.tversky_coeff(
+            half_window_size_px=DEFAULT_HALF_WINDOW_SIZE_PX,
+            false_positive_weight=1., false_negative_weight=1., test_mode=True
+        )
+        this_tversky_coeff = K.eval(
+            this_function(TARGET_TENSOR, PREDICTION_TENSOR)
+        )
+
+        self.assertTrue(0 <= this_tversky_coeff <= 1)
+
+    def test_focal_loss(self):
+        """Ensures that focal_loss() does not crash."""
+
+        this_function = custom_metrics.focal_loss(
+            half_window_size_px=DEFAULT_HALF_WINDOW_SIZE_PX,
+            training_event_freq=0.5, focusing_factor=1., test_mode=True
+        )
+        this_focal_loss = K.eval(
+            this_function(TARGET_TENSOR, PREDICTION_TENSOR)
+        )
+
+        self.assertTrue(this_focal_loss >= 0.)
 
 
 if __name__ == '__main__':
