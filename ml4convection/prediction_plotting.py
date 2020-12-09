c7f3b20ba6d4812d0ce5cfdb7012b4b7c737d312
thunderhoser <lagerqui@ualberta.ca>
2020-12-08 22:23:49 -0700

    Tweak.

--- ml4convection/prediction_plotting.py
+++ ml4convection/prediction_plotting.py
@@ -218,8 +218,8 @@ def plot_probabilistic(
         occurs.
     """
 
-    print(numpy.max(probability_matrix))
-    print(numpy.nanmax(probability_matrix))
+    # TODO(thunderhoser): This is a HACK.
+    probability_matrix = numpy.minimum(probability_matrix, 1.)
     error_checking.assert_is_geq_numpy_array(probability_matrix, 0.)
     error_checking.assert_is_leq_numpy_array(probability_matrix, 1.)
     error_checking.assert_is_numpy_array(probability_matrix, num_dimensions=2)
