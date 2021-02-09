a0cb20578daf2778567b71dbc67909aa72ed8d71
thunderhoser <lagerqui@ualberta.ca>
2021-02-09 14:15:44 -0700

    Done implementing CoordConv!

--- ml4convection/coord_conv.py
+++ ml4convection/coord_conv.py
@@ -5,6 +5,7 @@ https://arxiv.org/abs/1807.03247
 
 import os
 import sys
+import numpy
 from keras import backend as K
 
 THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
@@ -15,26 +16,37 @@ sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
 import error_checking
 
 
-def add_spatial_coords_2d(input_layer_object):
+def add_spatial_coords_2d(
+        input_layer_object, test_mode=False, input_dimensions=None):
     """Adds spatial coords to layer with two spatial dimensions.
 
     :param input_layer_object: Input layer (instance of `keras.layers.Layer`).
+    :param test_mode: Leave this alone.
+    :param input_dimensions: Leave this alone.
     :return: output_layer_object: Same but with two extra output channels, one
         for each spatial coordinate.
     """
 
-    input_dimensions = K.shape(input_layer_object)
-    input_dimensions = [input_dimensions[i] for i in range(4)]
+    error_checking.assert_is_boolean(test_mode)
 
-    # error_checking.assert_is_geq(len(input_dimensions), 4)
-    # error_checking.assert_is_leq(len(input_dimensions), 4)
+    if test_mode:
+        error_checking.assert_is_numpy_array(input_dimensions, num_dimensions=1)
+        error_checking.assert_is_integer_numpy_array(input_dimensions)
+        error_checking.assert_is_greater_numpy_array(input_dimensions, 0)
+
+        error_checking.assert_is_geq(len(input_dimensions), 4)
+        error_checking.assert_is_leq(len(input_dimensions), 4)
+    else:
+        input_dimensions = K.shape(input_layer_object)
+        input_dimensions = [input_dimensions[i] for i in range(4)]
 
     num_examples = input_dimensions[0]
     num_grid_rows = input_dimensions[1]
     num_grid_columns = input_dimensions[2]
 
     one_matrix_for_y = K.ones(
-        K.stack([num_examples, num_grid_columns]), dtype='int32'
+        K.stack([num_examples, num_grid_columns]),
+        dtype='int64' if test_mode else 'int32'
     )
     one_matrix_for_y = K.expand_dims(one_matrix_for_y, axis=-1)
 
@@ -52,7 +64,8 @@ def add_spatial_coords_2d(input_layer_object):
     y_coord_matrix = (y_coord_matrix * 2) - 1.
 
     one_matrix_for_x = K.ones(
-        K.stack([num_examples, num_grid_rows]), dtype='int32'
+        K.stack([num_examples, num_grid_rows]),
+        dtype='int64' if test_mode else 'int32'
     )
     one_matrix_for_x = K.expand_dims(one_matrix_for_x, axis=1)
 
@@ -70,38 +83,51 @@ def add_spatial_coords_2d(input_layer_object):
     x_coord_matrix = x_coord_matrix / K.cast(num_grid_columns - 1, K.floatx())
     x_coord_matrix = (x_coord_matrix * 2) - 1.
 
+    if test_mode:
+        return numpy.concatenate((x_coord_matrix, y_coord_matrix), axis=-1)
+
     return K.concatenate(
         [input_layer_object, x_coord_matrix, y_coord_matrix], axis=-1
     )
 
 
-def add_spatial_coords_2d_with_time(input_layer_object, num_times):
+def add_spatial_coords_2d_with_time(
+        input_layer_object, num_times, test_mode=False, input_dimensions=None):
     """Adds spatial coords to layer with two spatial dim and time dim.
 
     :param input_layer_object: Input layer (instance of `keras.layers.Layer`).
     :param num_times: Number of times.
+    :param test_mode: Leave this alone.
+    :param input_dimensions: Leave this alone.
     :return: output_layer_object: Same but with two extra output channels, one
         for each spatial coordinate.
     """
 
+    error_checking.assert_is_boolean(test_mode)
+
+    if test_mode:
+        error_checking.assert_is_numpy_array(input_dimensions, num_dimensions=1)
+        error_checking.assert_is_integer_numpy_array(input_dimensions)
+        error_checking.assert_is_greater_numpy_array(input_dimensions, 0)
+
+        error_checking.assert_is_geq(len(input_dimensions), 5)
+        error_checking.assert_is_leq(len(input_dimensions), 5)
+
+        num_times = input_dimensions[-2]
+    else:
+        input_dimensions = K.shape(input_layer_object)
+        input_dimensions = [input_dimensions[i] for i in range(5)]
+
     error_checking.assert_is_integer(num_times)
     error_checking.assert_is_geq(num_times, 1)
-
-    input_dimensions = K.shape(input_layer_object)
-    input_dimensions = [input_dimensions[i] for i in range(5)]
-
-    # error_checking.assert_is_geq(len(input_dimensions), 5)
-    # error_checking.assert_is_leq(len(input_dimensions), 5)
 
     num_examples = input_dimensions[0]
     num_grid_rows = input_dimensions[1]
     num_grid_columns = input_dimensions[2]
 
-    print(num_grid_columns)
-    print(num_times)
-
     one_matrix_for_y = K.ones(
-        K.stack([num_examples, num_grid_columns]), dtype='int32'
+        K.stack([num_examples, num_grid_columns]),
+        dtype='int64' if test_mode else 'int32'
     )
     one_matrix_for_y = K.expand_dims(one_matrix_for_y, axis=-1)
 
@@ -122,7 +148,8 @@ def add_spatial_coords_2d_with_time(input_layer_object, num_times):
     y_coord_matrix = (y_coord_matrix * 2) - 1.
 
     one_matrix_for_x = K.ones(
-        K.stack([num_examples, num_grid_rows]), dtype='int32'
+        K.stack([num_examples, num_grid_rows]),
+        dtype='int64' if test_mode else 'int32'
     )
     one_matrix_for_x = K.expand_dims(one_matrix_for_x, axis=1)
 
@@ -142,6 +169,9 @@ def add_spatial_coords_2d_with_time(input_layer_object, num_times):
     x_coord_matrix = K.cast(x_coord_matrix, K.floatx())
     x_coord_matrix = x_coord_matrix / K.cast(num_grid_columns - 1, K.floatx())
     x_coord_matrix = (x_coord_matrix * 2) - 1.
+
+    if test_mode:
+        return numpy.concatenate((x_coord_matrix, y_coord_matrix), axis=-1)
 
     return K.concatenate(
         [input_layer_object, x_coord_matrix, y_coord_matrix], axis=-1
