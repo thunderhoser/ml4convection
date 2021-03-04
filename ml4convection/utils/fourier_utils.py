c81def4f2460ef9d6c914bed5243e4ce5d491fea
thunderhoser <lagerqui@ualberta.ca>
2021-03-03 20:41:56 -0700

    Added fourier_utils.py.

---
+++ ml4convection/utils/fourier_utils.py
@@ -0,0 +1,230 @@
+"""Helper methods for Fourier transforms in 2-D space."""
+
+import numpy
+from gewittergefahr.gg_utils import error_checking
+
+TOLERANCE = 1e-6
+
+
+def _get_spatial_resolutions(half_num_rows, half_num_columns,
+                             grid_spacing_metres):
+    """Computes spatial resolution for each Fourier coefficient.
+
+    M = number of rows in spatial grid
+    N = number of columns in spatial grid
+
+    Matrices returned by this method correspond to matrices of Fourier
+    coefficients returned by `numpy.fft.fft2`.  The x-coordinate increases with
+    column index, and the y-coordinate increases with row index.
+
+    :param half_num_rows: Number of grid rows on either side of center.
+    :param half_num_columns: Number of grid columns on either side of center.
+    :param grid_spacing_metres: Grid spacing (resolution).
+    :return: x_resolution_matrix_metres: M-by-N numpy array of resolutions in
+        x-direction.
+    :return: y_resolution_matrix_metres: Same but for y-direction.
+    """
+
+    error_checking.assert_is_integer(half_num_rows)
+    error_checking.assert_is_geq(half_num_rows, 1)
+    error_checking.assert_is_integer(half_num_columns)
+    error_checking.assert_is_geq(half_num_columns, 1)
+    error_checking.assert_is_greater(grid_spacing_metres, 0.)
+
+    num_rows = 2 * half_num_rows + 1
+    num_columns = 2 * half_num_columns + 1
+
+    unique_x_wavenumbers = numpy.linspace(
+        0, half_num_columns, num=half_num_columns + 1, dtype=int
+    )
+    x_wavenumbers = numpy.concatenate((
+        unique_x_wavenumbers, unique_x_wavenumbers[1:]
+    ))
+    x_wavenumber_matrix = numpy.expand_dims(x_wavenumbers, axis=0)
+    x_wavenumber_matrix = numpy.repeat(
+        x_wavenumber_matrix, axis=0, repeats=num_rows
+    )
+
+    x_grid_length = grid_spacing_metres * (num_columns - 1)
+    x_resolution_matrix_metres = 0.5 * x_grid_length / x_wavenumber_matrix
+
+    unique_y_wavenumbers = numpy.linspace(
+        0, half_num_rows, num=half_num_rows + 1, dtype=int
+    )
+    y_wavenumbers = numpy.concatenate((
+        unique_y_wavenumbers, unique_y_wavenumbers[1:]
+    ))
+    y_wavenumber_matrix = numpy.expand_dims(y_wavenumbers, axis=1)
+    y_wavenumber_matrix = numpy.repeat(
+        y_wavenumber_matrix, axis=1, repeats=num_columns
+    )
+
+    y_grid_length = grid_spacing_metres * (num_rows - 1)
+    y_resolution_matrix_metres = 0.5 * y_grid_length / y_wavenumber_matrix
+
+    return x_resolution_matrix_metres, y_resolution_matrix_metres
+
+
+def apply_rectangular_filter(
+        coefficient_matrix, grid_spacing_metres, min_resolution_metres,
+        max_resolution_metres):
+    """Applies rectangular band-pass filter to Fourier coefficients.
+
+    M = number of rows in spatial grid
+    N = number of columns in spatial grid
+
+    :param coefficient_matrix: M-by-N numpy array of coefficients in format
+        returned by `numpy.fft.fft2`.
+    :param grid_spacing_metres: Grid spacing (resolution).
+    :param min_resolution_metres: Minimum resolution to preserve.
+    :param max_resolution_metres: Max resolution to preserve.
+    :return: coefficient_matrix: Same as input but maybe with some coefficients
+        zeroed out.
+    """
+
+    error_checking.assert_is_geq(min_resolution_metres, 0.)
+    error_checking.assert_is_greater(
+        max_resolution_metres, min_resolution_metres
+    )
+
+    # error_checking.assert_is_numpy_array_without_nan(coefficient_matrix)
+    error_checking.assert_is_numpy_array(coefficient_matrix, num_dimensions=2)
+
+    half_num_rows_float = float(coefficient_matrix.shape[0] - 1) / 2
+    half_num_rows = int(numpy.round(half_num_rows_float))
+    assert numpy.isclose(half_num_rows, half_num_rows_float, atol=TOLERANCE)
+
+    half_num_columns_float = float(coefficient_matrix.shape[1] - 1) / 2
+    half_num_columns = int(numpy.round(half_num_columns_float))
+    assert numpy.isclose(
+        half_num_columns, half_num_columns_float, atol=TOLERANCE
+    )
+
+    x_resolution_matrix_metres, y_resolution_matrix_metres = (
+        _get_spatial_resolutions(
+            half_num_rows=half_num_rows, half_num_columns=half_num_columns,
+            grid_spacing_metres=grid_spacing_metres
+        )
+    )
+
+    resolution_matrix_metres = numpy.sqrt(
+        x_resolution_matrix_metres ** 2 + y_resolution_matrix_metres ** 2
+    )
+
+    coefficient_matrix[resolution_matrix_metres > max_resolution_metres] = 0.
+    coefficient_matrix[resolution_matrix_metres < min_resolution_metres] = 0.
+    return coefficient_matrix
+
+
+def apply_butterworth_filter(
+        coefficient_matrix, filter_order, grid_spacing_metres,
+        min_resolution_metres, max_resolution_metres):
+    """Applies Butterworth band-pass filter to Fourier coefficients.
+
+    :param coefficient_matrix: See doc for `apply_rectangular_filter`.
+    :param filter_order: Order of Butterworth filter (same as input arg `N` for
+        `scipy.signal.butter`).
+    :param grid_spacing_metres: See doc for `apply_rectangular_filter`.
+    :param min_resolution_metres: Same.
+    :param max_resolution_metres: Same.
+    :return: coefficient_matrix: Same as input but maybe with some coefficients
+        zeroed out.
+    """
+
+    error_checking.assert_is_geq(filter_order, 1.)
+    error_checking.assert_is_geq(min_resolution_metres, 0.)
+    error_checking.assert_is_greater(
+        max_resolution_metres, min_resolution_metres
+    )
+
+    error_checking.assert_is_numpy_array(coefficient_matrix, num_dimensions=2)
+
+    # TODO(thunderhoser): Change dimension inputs to _get_spatial_resolutions.
+    half_num_rows_float = float(coefficient_matrix.shape[0] - 1) / 2
+    half_num_rows = int(numpy.round(half_num_rows_float))
+    assert numpy.isclose(half_num_rows, half_num_rows_float, atol=TOLERANCE)
+
+    half_num_columns_float = float(coefficient_matrix.shape[1] - 1) / 2
+    half_num_columns = int(numpy.round(half_num_columns_float))
+    assert numpy.isclose(
+        half_num_columns, half_num_columns_float, atol=TOLERANCE
+    )
+
+    x_resolution_matrix_metres, y_resolution_matrix_metres = (
+        _get_spatial_resolutions(
+            half_num_rows=half_num_rows, half_num_columns=half_num_columns,
+            grid_spacing_metres=grid_spacing_metres
+        )
+    )
+
+    x_wavenumber_matrix_metres01 = (2 * x_resolution_matrix_metres) ** -1
+    y_wavenumber_matrix_metres01 = (2 * y_resolution_matrix_metres) ** -1
+    wavenumber_matrix_metres01 = numpy.sqrt(
+        x_wavenumber_matrix_metres01 ** 2 + y_wavenumber_matrix_metres01 ** 2
+    )
+
+    # High-pass part.
+    if not numpy.isinf(max_resolution_metres):
+        min_wavenumber_metres01 = (2 * max_resolution_metres) ** -1
+        ratio_matrix = wavenumber_matrix_metres01 / min_wavenumber_metres01
+        gain_matrix = 1 - (1 + ratio_matrix ** (2 * filter_order)) ** -1
+        coefficient_matrix = coefficient_matrix * gain_matrix
+
+    # Low-pass part.
+    if min_resolution_metres > grid_spacing_metres:
+        max_wavenumber_metres01 = (2 * min_resolution_metres) ** -1
+        ratio_matrix = wavenumber_matrix_metres01 / max_wavenumber_metres01
+        gain_matrix = (1 + ratio_matrix ** (2 * filter_order)) ** -1
+        coefficient_matrix = coefficient_matrix * gain_matrix
+
+    return coefficient_matrix
+
+
+def apply_blackman_window(spatial_data_matrix):
+    """Applies Blackman window to 2-D spatial data.
+
+    M = number of rows in grid
+    N = number of columns in grid
+
+    :param spatial_data_matrix: M-by-N numpy array of real numbers.
+    :return: spatial_data_matrix: Same but after smoothing via Blackman window.
+    """
+
+    # TODO(thunderhoser): Implement non-radial version (separation of coords?)?
+
+    error_checking.assert_is_numpy_array_without_nan(spatial_data_matrix)
+    error_checking.assert_is_numpy_array(spatial_data_matrix, num_dimensions=2)
+
+    num_rows = spatial_data_matrix.shape[0]
+    num_columns = spatial_data_matrix.shape[1]
+    num_half_rows = float(num_rows - 1) / 2
+    num_half_columns = float(num_columns - 1) / 2
+
+    row_indices = numpy.linspace(0, num_rows - 1, num=num_rows, dtype=float)
+    column_indices = numpy.linspace(
+        0, num_columns - 1, num=num_columns, dtype=float
+    )
+
+    y_distances = numpy.absolute(row_indices - num_half_rows)
+    x_distances = numpy.absolute(column_indices - num_half_columns)
+    x_distance_matrix, y_distance_matrix = numpy.meshgrid(
+        x_distances, y_distances
+    )
+
+    distance_matrix = numpy.sqrt(
+        x_distance_matrix ** 2 + y_distance_matrix ** 2
+    )
+    max_distance = numpy.maximum(
+        numpy.max(x_distance_matrix),
+        numpy.max(y_distance_matrix)
+    )
+    fractional_distance_matrix = distance_matrix / max_distance
+    fractional_distance_matrix = numpy.minimum(fractional_distance_matrix, 1.)
+
+    weight_matrix = (
+        0.42 -
+        0.5 * numpy.cos(numpy.pi * (1 + fractional_distance_matrix)) +
+        0.08 * numpy.cos(2 * numpy.pi * (1 + fractional_distance_matrix))
+    )
+
+    return spatial_data_matrix * weight_matrix
