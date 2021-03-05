070045b66ac1d1381d50f4b404aed4aee5ae337b
thunderhoser <lagerqui@ualberta.ca>
2021-03-04 18:59:21 -0700

    Added Fourier shit.

---
+++ ml4convection/fourier_metrics.py
@@ -0,0 +1,194 @@
+"""Scale-separation-based metrics (with Fourier decomp) for Keras models."""
+
+import os
+import sys
+import numpy
+import tensorflow
+from tensorflow.keras import backend as K
+
+THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))
+))
+sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
+
+import error_checking
+import fourier_utils
+
+SPATIAL_COEFFS_KEY = 'spatial_coeff_matrix'
+FREQUENCY_COEFFS_KEY = 'frequency_coeff_matrix'
+MASK_KEY = 'mask_matrix'
+ORIG_NUM_ROWS_KEY = 'orig_num_rows'
+ORIG_NUM_COLUMNS_KEY = 'orig_num_columns'
+
+
+def _check_input_args(
+        spatial_coeff_matrix, frequency_coeff_matrix, mask_matrix,
+        function_name):
+    """Error-checks input arguments for any metric.
+
+    M = number of rows in spatial grid
+    N = number of columns in spatial grid
+
+    :param spatial_coeff_matrix: numpy array (3M x 3N) of coefficients for
+        window function, to be applied in spatial domain.
+    :param frequency_coeff_matrix: numpy array (3M x 3N) of coefficients for
+        filter function, to be applied in frequency domain.
+    :param mask_matrix: M-by-N numpy array of Boolean flags.  Grid points marked
+        "False" are masked out and not used to compute the metric.
+    :param function_name: Function name (string).  May be None.
+    :return: argument_dict: Dictionary with the following keys.
+    argument_dict['spatial_coeff_matrix']: Same as input but with dimensions of
+        1 x 3M x 3N.
+    argument_dict['frequency_coeff_matrix']: Same as input but with dimensions
+        of 1 x 3M x 3N.
+    argument_dict['mask_matrix']: Same as input but with dimensions of
+        1 x 3M x 3N.
+    argument_dict['orig_num_rows']: M in the above discussion.
+    argument_dict['orig_num_columns']: N in the above discussion.
+    """
+
+    error_checking.assert_is_boolean_numpy_array(mask_matrix)
+    error_checking.assert_is_numpy_array(mask_matrix, num_dimensions=2)
+
+    orig_num_rows = mask_matrix.shape[0]
+    orig_num_columns = mask_matrix.shape[1]
+    expected_dim = numpy.array(
+        [3 * orig_num_rows, 3 * orig_num_columns], dtype=int
+    )
+
+    error_checking.assert_is_numpy_array_without_nan(spatial_coeff_matrix)
+    error_checking.assert_is_numpy_array(
+        spatial_coeff_matrix, exact_dimensions=expected_dim
+    )
+
+    error_checking.assert_is_numpy_array_without_nan(frequency_coeff_matrix)
+    error_checking.assert_is_numpy_array(
+        frequency_coeff_matrix, exact_dimensions=expected_dim
+    )
+
+    if function_name is not None:
+        error_checking.assert_is_string(function_name)
+
+    spatial_coeff_matrix = numpy.expand_dims(spatial_coeff_matrix, axis=0)
+    frequency_coeff_matrix = numpy.expand_dims(frequency_coeff_matrix, axis=0)
+    mask_matrix = fourier_utils.taper_spatial_data(mask_matrix.astype(float))
+    mask_matrix = numpy.expand_dims(mask_matrix, axis=0).astype(numpy.float32)
+
+    return {
+        SPATIAL_COEFFS_KEY: spatial_coeff_matrix,
+        FREQUENCY_COEFFS_KEY: frequency_coeff_matrix,
+        MASK_KEY: mask_matrix,
+        ORIG_NUM_ROWS_KEY: orig_num_rows,
+        ORIG_NUM_COLUMNS_KEY: orig_num_columns
+    }
+
+
+def _filter_fields(
+        target_tensor, prediction_tensor, spatial_coeff_matrix,
+        frequency_coeff_matrix, orig_num_rows, orig_num_columns):
+    """Filters predicted and target fields via Fourier transform, then inverse.
+
+    :param target_tensor: Tensor of target (actual) values.
+    :param prediction_tensor: Tensor of predicted values.
+    :param spatial_coeff_matrix: See output doc for `_check_input_args`.
+    :param frequency_coeff_matrix: Same.
+    :param orig_num_rows: Same.
+    :param orig_num_columns: Same.
+    :return: target_tensor: Filtered version of input.
+    :return: prediction_tensor: Filtered version of input.
+    """
+
+    padding_arg = (
+        (orig_num_rows, orig_num_rows),
+        (orig_num_columns, orig_num_columns)
+    )
+
+    target_tensor = K.spatial_2d_padding(
+        target_tensor, padding=padding_arg, data_format='channels_last'
+    )
+    target_tensor = spatial_coeff_matrix * target_tensor[..., 0]
+
+    target_coeff_tensor = frequency_coeff_matrix * tensorflow.signal.fft2d(
+        K.cast(target_tensor, dtype=tensorflow.complex64)
+    )
+
+    target_tensor = tensorflow.math.real(
+        tensorflow.signal.ifft2d(target_coeff_tensor)
+    )
+    target_tensor = K.maximum(target_tensor, 0.)
+    target_tensor = K.minimum(target_tensor, 1.)
+
+    prediction_tensor = K.spatial_2d_padding(
+        prediction_tensor, padding=padding_arg, data_format='channels_last'
+    )
+    prediction_tensor = spatial_coeff_matrix * prediction_tensor[..., 0]
+
+    predicted_coeff_tensor = (
+        frequency_coeff_matrix *
+        tensorflow.signal.fft2d(
+            K.cast(prediction_tensor, dtype=tensorflow.complex64)
+        )
+    )
+
+    prediction_tensor = tensorflow.math.real(
+        tensorflow.signal.ifft2d(predicted_coeff_tensor)
+    )
+    prediction_tensor = K.maximum(prediction_tensor, 0.)
+    prediction_tensor = K.minimum(prediction_tensor, 1.)
+
+    return target_tensor, prediction_tensor
+
+
+def mean_squared_error(
+        spatial_coeff_matrix, frequency_coeff_matrix, mask_matrix,
+        function_name=None):
+    """Creates function to compute mean squared error at a given scale.
+
+    :param spatial_coeff_matrix: See doc for `_check_input_args`.
+    :param frequency_coeff_matrix: Same.
+    :param mask_matrix: Same.
+    :param function_name: Same.
+    :return: mse_function: Function (defined below).
+    """
+
+    argument_dict = _check_input_args(
+        spatial_coeff_matrix=spatial_coeff_matrix,
+        frequency_coeff_matrix=frequency_coeff_matrix,
+        mask_matrix=mask_matrix, function_name=function_name
+    )
+
+    spatial_coeff_matrix = argument_dict[SPATIAL_COEFFS_KEY]
+    frequency_coeff_matrix = argument_dict[FREQUENCY_COEFFS_KEY]
+    mask_matrix = argument_dict[MASK_KEY]
+    orig_num_rows = argument_dict[ORIG_NUM_ROWS_KEY]
+    orig_num_columns = argument_dict[ORIG_NUM_COLUMNS_KEY]
+
+    def mse_function(target_tensor, prediction_tensor):
+        """Computes mean squared error (MSE) at a given scale.
+
+        :param target_tensor: Tensor of target (actual) values.
+        :param prediction_tensor: Tensor of predicted values.
+        :return: mse_value: MSE.
+        """
+
+        _filter_fields(
+            target_tensor=target_tensor, prediction_tensor=prediction_tensor,
+            spatial_coeff_matrix=spatial_coeff_matrix,
+            frequency_coeff_matrix=frequency_coeff_matrix,
+            orig_num_rows=orig_num_rows, orig_num_columns=orig_num_columns
+        )
+
+        squared_error_tensor = K.sum(
+            (target_tensor - prediction_tensor) ** 2,
+            axis=(1, 2)
+        )
+
+        mask_tensor = mask_matrix * K.ones_like(prediction_tensor)
+        num_pixels_tensor = K.sum(mask_tensor, axis=(1, 2))
+
+        return K.mean(squared_error_tensor / num_pixels_tensor)
+
+    if function_name is not None:
+        mse_function.__name__ = function_name
+
+    return mse_function
---
+++ ml4convection/fourier_utils.py
@@ -0,0 +1,309 @@
+"""Helper methods for Fourier transforms in 2-D space."""
+
+import os
+import sys
+import numpy
+
+THIS_DIRECTORY_NAME = os.path.dirname(os.path.realpath(
+    os.path.join(os.getcwd(), os.path.expanduser(__file__))
+))
+sys.path.append(os.path.normpath(os.path.join(THIS_DIRECTORY_NAME, '..')))
+
+import error_checking
+
+TOLERANCE = 1e-6
+
+
+def _get_spatial_resolutions(num_grid_rows, num_grid_columns,
+                             grid_spacing_metres):
+    """Computes spatial resolution for each Fourier coefficient.
+
+    M = number of rows in spatial grid
+    N = number of columns in spatial grid
+
+    Matrices returned by this method correspond to matrices of Fourier
+    coefficients returned by `numpy.fft.fft2`.  The x-coordinate increases with
+    column index, and the y-coordinate increases with row index.
+
+    :param num_grid_rows: M in the above discussion.
+    :param num_grid_columns: N in the above discussion.
+    :param grid_spacing_metres: Grid spacing (for which I use "resolution" as a
+        synonym).
+    :return: x_resolution_matrix_metres: M-by-N numpy array of resolutions in
+        x-direction.
+    :return: y_resolution_matrix_metres: Same but for y-direction.
+    """
+
+    # Check input args.
+    error_checking.assert_is_integer(num_grid_rows)
+    error_checking.assert_is_geq(num_grid_rows, 3)
+    error_checking.assert_is_integer(num_grid_columns)
+    error_checking.assert_is_geq(num_grid_columns, 3)
+    error_checking.assert_is_greater(grid_spacing_metres, 0.)
+
+    num_half_rows_float = float(num_grid_rows - 1) / 2
+    num_half_rows = int(numpy.round(num_half_rows_float))
+    assert numpy.isclose(num_half_rows, num_half_rows_float, atol=TOLERANCE)
+
+    num_half_columns_float = float(num_grid_columns - 1) / 2
+    num_half_columns = int(numpy.round(num_half_columns_float))
+    assert numpy.isclose(
+        num_half_columns, num_half_columns_float, atol=TOLERANCE
+    )
+
+    # Find resolutions in x-direction.
+    unique_x_wavenumbers = numpy.linspace(
+        0, num_half_columns, num=num_half_columns + 1, dtype=int
+    )
+    x_wavenumbers = numpy.concatenate((
+        unique_x_wavenumbers, unique_x_wavenumbers[1:]
+    ))
+    x_wavenumber_matrix = numpy.expand_dims(x_wavenumbers, axis=0)
+    x_wavenumber_matrix = numpy.repeat(
+        x_wavenumber_matrix, axis=0, repeats=num_grid_rows
+    )
+
+    x_grid_length_metres = grid_spacing_metres * (num_grid_columns - 1)
+    x_resolution_matrix_metres = (
+        0.5 * x_grid_length_metres / x_wavenumber_matrix
+    )
+
+    # Find resolutions in y-direction.
+    unique_y_wavenumbers = numpy.linspace(
+        0, num_half_rows, num=num_half_rows + 1, dtype=int
+    )
+    y_wavenumbers = numpy.concatenate((
+        unique_y_wavenumbers, unique_y_wavenumbers[1:]
+    ))
+    y_wavenumber_matrix = numpy.expand_dims(y_wavenumbers, axis=1)
+    y_wavenumber_matrix = numpy.repeat(
+        y_wavenumber_matrix, axis=1, repeats=num_grid_columns
+    )
+
+    y_grid_length_metres = grid_spacing_metres * (num_grid_rows - 1)
+    y_resolution_matrix_metres = (
+        0.5 * y_grid_length_metres / y_wavenumber_matrix
+    )
+
+    return x_resolution_matrix_metres, y_resolution_matrix_metres
+
+
+def apply_rectangular_filter(
+        coefficient_matrix, grid_spacing_metres, min_resolution_metres,
+        max_resolution_metres):
+    """Applies rectangular band-pass filter to Fourier coefficients.
+
+    M = number of rows in spatial grid
+    N = number of columns in spatial grid
+
+    :param coefficient_matrix: M-by-N numpy array of coefficients in format
+        returned by `numpy.fft.fft2`.
+    :param grid_spacing_metres: Grid spacing (resolution).
+    :param min_resolution_metres: Minimum resolution to preserve.
+    :param max_resolution_metres: Max resolution to preserve.
+    :return: coefficient_matrix: Same as input but maybe with some coefficients
+        zeroed out.
+    """
+
+    # Check input args.
+    error_checking.assert_is_geq(min_resolution_metres, 0.)
+    error_checking.assert_is_greater(
+        max_resolution_metres, min_resolution_metres
+    )
+
+    error_checking.assert_is_numpy_array(coefficient_matrix, num_dimensions=2)
+    error_checking.assert_is_numpy_array_without_nan(
+        numpy.real(coefficient_matrix)
+    )
+    error_checking.assert_is_numpy_array_without_nan(
+        numpy.imag(coefficient_matrix)
+    )
+
+    # Do actual stuff.
+    x_resolution_matrix_metres, y_resolution_matrix_metres = (
+        _get_spatial_resolutions(
+            num_grid_rows=coefficient_matrix.shape[0],
+            num_grid_columns=coefficient_matrix.shape[1],
+            grid_spacing_metres=grid_spacing_metres
+        )
+    )
+
+    resolution_matrix_metres = numpy.sqrt(
+        x_resolution_matrix_metres ** 2 + y_resolution_matrix_metres ** 2
+    )
+
+    coefficient_matrix[resolution_matrix_metres > max_resolution_metres] = 0.
+    coefficient_matrix[resolution_matrix_metres < min_resolution_metres] = 0.
+    return coefficient_matrix
+
+
+def apply_butterworth_filter(
+        coefficient_matrix, filter_order, grid_spacing_metres,
+        min_resolution_metres, max_resolution_metres):
+    """Applies Butterworth band-pass filter to Fourier coefficients.
+
+    :param coefficient_matrix: See doc for `apply_rectangular_filter`.
+    :param filter_order: Order of Butterworth filter (same as input arg `N` for
+        `scipy.signal.butter`).
+    :param grid_spacing_metres: See doc for `apply_rectangular_filter`.
+    :param min_resolution_metres: Same.
+    :param max_resolution_metres: Same.
+    :return: coefficient_matrix: Same as input but after filtering.
+    """
+
+    # Check input args.
+    error_checking.assert_is_geq(filter_order, 1.)
+    error_checking.assert_is_geq(min_resolution_metres, 0.)
+    error_checking.assert_is_greater(
+        max_resolution_metres, min_resolution_metres
+    )
+
+    error_checking.assert_is_numpy_array(coefficient_matrix, num_dimensions=2)
+    error_checking.assert_is_numpy_array_without_nan(
+        numpy.real(coefficient_matrix)
+    )
+    error_checking.assert_is_numpy_array_without_nan(
+        numpy.imag(coefficient_matrix)
+    )
+
+    # Determine horizontal, vertical, and total wavenumber for each Fourier
+    # coefficient.
+    x_resolution_matrix_metres, y_resolution_matrix_metres = (
+        _get_spatial_resolutions(
+            num_grid_rows=coefficient_matrix.shape[0],
+            num_grid_columns=coefficient_matrix.shape[1],
+            grid_spacing_metres=grid_spacing_metres
+        )
+    )
+
+    x_wavenumber_matrix_metres01 = (2 * x_resolution_matrix_metres) ** -1
+    y_wavenumber_matrix_metres01 = (2 * y_resolution_matrix_metres) ** -1
+    wavenumber_matrix_metres01 = numpy.sqrt(
+        x_wavenumber_matrix_metres01 ** 2 + y_wavenumber_matrix_metres01 ** 2
+    )
+
+    # High-pass part.
+    if not numpy.isinf(max_resolution_metres):
+        min_wavenumber_metres01 = (2 * max_resolution_metres) ** -1
+        ratio_matrix = wavenumber_matrix_metres01 / min_wavenumber_metres01
+        gain_matrix = 1 - (1 + ratio_matrix ** (2 * filter_order)) ** -1
+        coefficient_matrix = coefficient_matrix * gain_matrix
+
+    # Low-pass part.
+    if min_resolution_metres > grid_spacing_metres:
+        max_wavenumber_metres01 = (2 * min_resolution_metres) ** -1
+        ratio_matrix = wavenumber_matrix_metres01 / max_wavenumber_metres01
+        gain_matrix = (1 + ratio_matrix ** (2 * filter_order)) ** -1
+        coefficient_matrix = coefficient_matrix * gain_matrix
+
+    return coefficient_matrix
+
+
+def taper_spatial_data(spatial_data_matrix):
+    """Tapers spatial data by putting zeros along the edge.
+
+    M = number of rows in grid
+    N = number of columns in grid
+
+    :param spatial_data_matrix: M-by-N numpy array of real numbers.
+    :return: spatial_data_matrix: Same but after tapering.
+    """
+
+    error_checking.assert_is_numpy_array_without_nan(spatial_data_matrix)
+    error_checking.assert_is_numpy_array(spatial_data_matrix, num_dimensions=2)
+
+    num_rows = spatial_data_matrix.shape[0]
+    num_columns = spatial_data_matrix.shape[1]
+
+    padding_arg = (
+        (num_rows, num_rows),
+        (num_columns, num_columns)
+    )
+
+    spatial_data_matrix = numpy.pad(
+        spatial_data_matrix, pad_width=padding_arg, mode='constant',
+        constant_values=0.
+    )
+
+    return spatial_data_matrix
+
+
+def untaper_spatial_data(spatial_data_matrix):
+    """Removes zeros along the edge of spatial data.
+
+    This method is the inverse of `taper_spatial_data`.
+
+    :param spatial_data_matrix: See output doc for `taper_spatial_data`.
+    :return: spatial_data_matrix: See input doc for `taper_spatial_data`.
+    """
+
+    error_checking.assert_is_numpy_array_without_nan(spatial_data_matrix)
+    error_checking.assert_is_numpy_array(spatial_data_matrix, num_dimensions=2)
+
+    num_rows = spatial_data_matrix.shape[0]
+    num_columns = spatial_data_matrix.shape[1]
+
+    num_third_rows_float = float(num_rows) / 3
+    num_third_rows = int(numpy.round(num_third_rows_float))
+    assert numpy.isclose(num_third_rows, num_third_rows_float, atol=TOLERANCE)
+
+    num_third_columns_float = float(num_columns) / 3
+    num_third_columns = int(numpy.round(num_third_columns_float))
+    assert numpy.isclose(
+        num_third_columns, num_third_columns_float, atol=TOLERANCE
+    )
+
+    return spatial_data_matrix[
+        num_third_rows:-num_third_rows,
+        num_third_columns:-num_third_columns
+    ]
+
+
+def apply_blackman_window(spatial_data_matrix):
+    """Applies Blackman window to 2-D spatial data.
+
+    M = number of rows in grid
+    N = number of columns in grid
+
+    :param spatial_data_matrix: M-by-N numpy array of real numbers.
+    :return: spatial_data_matrix: Same but after smoothing via Blackman window.
+    """
+
+    # TODO(thunderhoser): Implement non-radial version (separation of coords?)?
+
+    error_checking.assert_is_numpy_array_without_nan(spatial_data_matrix)
+    error_checking.assert_is_numpy_array(spatial_data_matrix, num_dimensions=2)
+
+    num_rows = spatial_data_matrix.shape[0]
+    num_columns = spatial_data_matrix.shape[1]
+    num_half_rows = float(num_rows - 1) / 2
+    num_half_columns = float(num_columns - 1) / 2
+
+    row_indices = numpy.linspace(0, num_rows - 1, num=num_rows, dtype=float)
+    column_indices = numpy.linspace(
+        0, num_columns - 1, num=num_columns, dtype=float
+    )
+
+    y_distances = numpy.absolute(row_indices - num_half_rows)
+    x_distances = numpy.absolute(column_indices - num_half_columns)
+    x_distance_matrix, y_distance_matrix = numpy.meshgrid(
+        x_distances, y_distances
+    )
+
+    distance_matrix = numpy.sqrt(
+        x_distance_matrix ** 2 + y_distance_matrix ** 2
+    )
+    max_distance = numpy.maximum(
+        numpy.max(x_distance_matrix),
+        numpy.max(y_distance_matrix)
+    )
+    fractional_distance_matrix = distance_matrix / max_distance
+    fractional_distance_matrix = numpy.minimum(fractional_distance_matrix, 1.)
+
+    weight_matrix = (
+        0.42 -
+        0.5 * numpy.cos(numpy.pi * (1 + fractional_distance_matrix)) +
+        0.08 * numpy.cos(2 * numpy.pi * (1 + fractional_distance_matrix))
+    )
+
+    return spatial_data_matrix * weight_matrix
