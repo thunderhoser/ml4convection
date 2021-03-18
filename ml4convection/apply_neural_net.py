7ccb69855b8027c581e06c5c8e4783d60eb51b0f
thunderhoser <lagerqui@ualberta.ca>
2021-03-18 13:37:49 -0600

    FOO

--- ml4convection/apply_neural_net.py
+++ ml4convection/apply_neural_net.py
@@ -271,8 +271,7 @@ def _run(model_file_name, top_predictor_dir_name, top_target_dir_name,
             training_option_dict[neural_net.NORMALIZE_FLAG_KEY],
         neural_net.UNIFORMIZE_FLAG_KEY:
             training_option_dict[neural_net.UNIFORMIZE_FLAG_KEY],
-        neural_net.ADD_COORDS_KEY:
-            training_option_dict[neural_net.ADD_COORDS_KEY]
+        neural_net.ADD_COORDS_KEY: False
     }
 
     valid_date_strings = time_conversion.get_spc_dates_in_range(
