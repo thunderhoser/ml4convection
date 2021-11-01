5b630263afac338751580a2387d66468d1df472e
thunderhoser <lagerqui@ualberta.ca>
2021-11-01 16:52:09 -0600

    FOO

--- ml4convection/rank_learning_curves_exp4to6_simpler.py
+++ ml4convection/rank_learning_curves_exp4to6_simpler.py
@@ -454,7 +454,8 @@ def _run(all_experiment_dir_name, output_dir_name):
             numpy.unravel_index(this_index, rank_matrix.shape)
         )
 
-        axes_object.set_ylabel('Score for loss function', loc='bottom')
+        axes_object.set_ylabel('Score for loss function')
+        axes_object.yaxis.set_label_coords(-0.1, 0.1)
         axes_object.set_xlabel('Filter for loss function')
 
         score_string = '{0:s} ranking'.format(LOSS_FUNCTION_NAMES_FANCY[i])
