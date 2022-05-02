285689040c737ddc14a29ad0f27b095b02f8ddd9
thunderhoser <lagerqui@ualberta.ca>
2022-05-02 10:23:02 -0600

    FOO

--- ml4convection/neural_net.py
+++ ml4convection/neural_net.py
@@ -2391,6 +2391,12 @@ def read_model(hdf5_file_name, for_mirrored_training=False):
                 metrics=metric_list
             )
     else:
+        print(custom_object_dict)
+        print('\n\n')
+        print(custom_object_dict['loss'])
+        print('\n\n')
+        print(metric_list)
+
         model_object.compile(
             loss=custom_object_dict['loss'], optimizer=keras.optimizers.Adam(),
             metrics=metric_list
