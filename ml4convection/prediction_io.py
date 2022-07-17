1b55fe7b4115c07a2c5deb4ead2604310cc51405
thunderhoser <lagerqui@ualberta.ca>
2022-07-17 12:51:48 -0600

    FOO

--- ml4convection/prediction_io.py
+++ ml4convection/prediction_io.py
@@ -304,7 +304,7 @@ def write_file(
     print(target_matrix.dtype)
     print(numpy.unique(target_matrix))
     dataset_object.createVariable(
-        TARGET_MATRIX_KEY, datatype='u4', dimensions=these_dim
+        TARGET_MATRIX_KEY, datatype=numpy.int32, dimensions=these_dim
     )
     dataset_object.variables[TARGET_MATRIX_KEY][:] = target_matrix
 
@@ -321,7 +321,7 @@ def write_file(
 
     # valid_times_unix_sec = valid_times_unix_sec.astype(int)
     dataset_object.createVariable(
-        VALID_TIMES_KEY, datatype='u4', dimensions=EXAMPLE_DIMENSION_KEY
+        VALID_TIMES_KEY, datatype=numpy.int32, dimensions=EXAMPLE_DIMENSION_KEY
     )
     dataset_object.variables[VALID_TIMES_KEY][:] = valid_times_unix_sec
 
